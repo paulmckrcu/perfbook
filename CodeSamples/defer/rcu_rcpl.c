2d29dc899c42c81f8a505dd37f7f96e8da2ea25c
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2008-11-01 18:21:10 -0700

    Add rcu_rcpl and a description of its implementation.
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

---
+++ CodeSamples/defer/rcu_rcpl.c
@@ -0,0 +1,62 @@
+/*
+ * rcu_rcpl.c: simple user-level implementation of RCU based on per-thread
+ * pairs of global reference counters.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Copyright (c) 2008 Paul E. McKenney, IBM Corporation.
+ */
+
+#include "../api.h"
+#include "rcu_rcpl.h"
+
+static void flip_counter_and_wait(int i)
+{
+	int t;
+
+	atomic_set(&rcu_idx, !i);
+	smp_mb();
+	for_each_thread(t) {
+		while (per_thread(rcu_refcnt, t)[i] != 0) {
+			poll(NULL, 0, 10);
+		}
+	}
+	smp_mb();
+}
+
+void synchronize_rcu(void)
+{
+	int i;
+
+	smp_mb();
+	spin_lock(&rcu_gp_lock);
+	i = atomic_read(&rcu_idx);
+
+	/*
+	 * Flip counter once and wait for old counts to go away,
+	 * but someone might have been preempted while we waited, so
+	 * we must flip and wait twice.
+	 */
+
+	flip_counter_and_wait(i);
+	flip_counter_and_wait(!i);
+
+	spin_unlock(&rcu_gp_lock);
+	smp_mb();
+}
+
+#ifdef TEST
+#include "rcutorture.h"
+#endif /* #ifdef TEST */
---
+++ CodeSamples/defer/rcu_rcpl.h
@@ -0,0 +1,71 @@
+/*
+ * rcu_rcpl.h: simple user-level implementation of RCU based on per-thread
+ * pairs of global reference counters.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Copyright (c) 2008 Paul E. McKenney, IBM Corporation.
+ */
+
+#include "rcu_pointer.h"
+
+DEFINE_SPINLOCK(rcu_gp_lock);
+DEFINE_PER_THREAD(int [2], rcu_refcnt);
+atomic_t rcu_idx;
+DEFINE_PER_THREAD(int, rcu_nesting);
+DEFINE_PER_THREAD(int, rcu_read_idx);
+
+static void rcu_init(void)
+{
+	int t;
+
+	atomic_set(&rcu_idx, 0);
+	init_per_thread(rcu_nesting, 0);
+	for_each_thread(t) {
+		per_thread(rcu_refcnt, t)[0] = 0;
+		per_thread(rcu_refcnt, t)[1] = 0;
+	}
+}
+
+static void rcu_read_lock(void)
+{
+	int i;
+	int n;
+
+	n = __get_thread_var(rcu_nesting);
+	if (n == 0) {
+		i = atomic_read(&rcu_idx);
+		__get_thread_var(rcu_read_idx) = i;
+		__get_thread_var(rcu_refcnt)[i]++;
+	}
+	__get_thread_var(rcu_nesting) = n + 1;
+	smp_mb();
+}
+
+static void rcu_read_unlock(void)
+{
+	int i;
+	int n;
+
+	smp_mb();
+	n = __get_thread_var(rcu_nesting);
+	if (n == 1) {
+		 i = __get_thread_var(rcu_read_idx);
+		__get_thread_var(rcu_refcnt)[i]--;
+	}
+	__get_thread_var(rcu_nesting) = n - 1;
+}
+
+extern void synchronize_rcu(void);
