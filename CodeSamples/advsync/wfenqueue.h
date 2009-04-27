9983c89506ea708aa9608a980a886c0bad8e72e1
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2009-04-26 18:25:01 -0700

    Give credit to MCS lock.
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- CodeSamples/advsync/wfenqueue.h
+++ CodeSamples/advsync/wfenqueue.h
@@ -1,6 +1,8 @@
 /*
  * wfenqueue.h: Simple queue with wait-free enqueue and blocking dequeue.
  *
+ * This is similar to the enqueueing operation used in the MCS lock.
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
