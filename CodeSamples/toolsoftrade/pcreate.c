9cf8421d298d445c9b7b92b505636fbe0bdd8dcb
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2009-05-09 18:45:50 -0700

    Add pcreate.c and corresponding discussion in toolsoftrade.
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

---
+++ CodeSamples/toolsoftrade/pcreate.c
@@ -0,0 +1,56 @@
+/*
+ * pcreate.c: Demonstrate use of POSIX pthread_create() and pthread_join()
+ *	primitives for parallel processing.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Copyright (c) 2009 Paul E. McKenney, IBM Corporation.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include "../api.h"
+
+int x = 0;
+
+void *mythread(void *arg)
+{
+	x = 1;
+	printf("Child process set x=1\n");
+	return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t tid;
+	void *vp;
+
+	if (pthread_create(&tid, NULL, mythread, NULL) != 0) {
+		perror("pthread_create");
+		exit(-1);
+	}
+
+	/* parent */
+
+	if (pthread_join(tid, &vp) != 0) {
+		perror("pthread_join");
+		exit(-1);
+	}
+	printf("Parent process sees x=%d\n", x);
+
+	return 0;
+}
