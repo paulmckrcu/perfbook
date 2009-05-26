1987e45445f5699f5f9fb2f4361efc9ec7f5aa00
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2009-05-25 23:26:13 -0700

    Add a couple more test programs and a bit more verbiage to toolsoftrade.
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- CodeSamples/toolsoftrade/forkexec.sh
+++ CodeSamples/toolsoftrade/forkexec.sh
@@ -1,4 +1,6 @@
-#! /bin/sh
+#!/bin/sh
+#
+# Example parallel and pipelined shell script.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,25 +20,7 @@
 #
 # Authors: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
 
-fontsize=10
-plotsize=0.7
-
-gnuplot << ---EOF---
-set term postscript portrait ${fontsize}
-set size square ${plotsize},${plotsize}
-set output "|../../utilities/gnuplotepsfix.7 > rwlockscale.eps"
-set xlabel "Number of CPUs (Threads)"
-set ylabel "Critical Section Performance"
-#set logscale y
-#set yrange [1:10000]
-#set yrange [100:10000]
-set nokey
-set label 1 "ideal" at 120,0.97 left
-set label 2 "100M" at 120,0.67 left
-set label 3 "10M" at 120,0.52 left
-set label 4 "1M" at 120,0.27 left
-set label 5 "100K" at 120,0.1 left
-set label 6 "10K" at 20,0.2 left
-set label 7 "1K" at 18,0.03 right
-plot "data/rwlockscale.dat" w e, "data/rwlockscale.dat" w l, 1
----EOF---
+for ((i=0;i<10000;i++))
+do
+	./mytrue
+done
