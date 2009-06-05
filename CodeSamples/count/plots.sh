9a45e0dd58c507cae8b584c86b21f17ea5e91fd2
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2009-06-04 21:50:50 -0700

    Add data from counting runs.
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- CodeSamples/count/plots.sh
+++ CodeSamples/count/plots.sh
@@ -20,3 +20,45 @@ set nokey
 # set label 5 "refcnt" at 0.15,2.8 left
 plot "data/count_atomic:u.2009.05.03a.dat" w e, "data/count_atomic:u.2009.05.03a.dat" w l, 8.81772
 ---EOF---
+
+gnuplot << ---EOF---
+set term postscript portrait ${fontsize}
+set size square ${plotsize},${plotsize}
+set output "|../../utilities/gnuplotepsfix > atomic125.eps"
+set xlabel "Number of CPUs/Threads"
+set ylabel "Time Per Increment (ns)"
+#set logscale y
+#set yrange [1:10000]
+#set yrange [100:10000]
+set nokey
+# set label 1 "rcu" at 0.1,10 left
+# set label 2 "spinlock" at 0.5,3.0 left
+# set label 3 "brlock" at 0.4,0.6 left
+# set label 4 "rwlock" at 0.3,1.6 left
+# set label 5 "refcnt" at 0.15,2.8 left
+plot "data/count_atomic\:u.2009.05.25a.dat" w e, "data/count_atomic\:u.2009.05.25a.dat" w l, 8.81772
+set term png medium
+set output "atomic125.png"
+replot
+---EOF---
+
+gnuplot << ---EOF---
+set term postscript portrait ${fontsize}
+set size square ${plotsize},${plotsize}
+set output "|../../utilities/gnuplotepsfix > atomic_nehalem.eps"
+set xlabel "Number of CPUs/Threads"
+set ylabel "Time Per Increment (ns)"
+#set logscale y
+#set yrange [1:10000]
+#set yrange [100:10000]
+set nokey
+# set label 1 "rcu" at 0.1,10 left
+# set label 2 "spinlock" at 0.5,3.0 left
+# set label 3 "brlock" at 0.4,0.6 left
+# set label 4 "rwlock" at 0.3,1.6 left
+# set label 5 "refcnt" at 0.15,2.8 left
+plot "data/count_atomic:u.elm3b128.2009.05.29a.dat" w e, "data/count_atomic:u.elm3b128.2009.05.29a.dat" w l
+set term png medium
+set output "atomic_nehalem.png"
+replot
+---EOF---
