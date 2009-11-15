1cdca2a4184717f78964e57b3b233043b7b8713a
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2009-11-14 22:23:07 -0800

    Add count_lim_app.c, which limits per-thread counter value.
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

---
+++ CodeSamples/count/count_lim_app.c
@@ -0,0 +1,128 @@
+/*
+ * count_lim_app.c: simple limit counter with degree of approximation
+ * 	controlled by MAX_COUNTERMAX.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Copyright (c) 2009 Paul E. McKenney, IBM Corporation.
+ */
+
+#include "../api.h"
+
+unsigned long __thread counter = 0;
+unsigned long __thread countermax = 0;
+unsigned long globalcountmax = 10000;
+unsigned long globalcount = 0;
+unsigned long globalreserve = 0;
+unsigned long *counterp[NR_THREADS] = { NULL };
+DEFINE_SPINLOCK(gblcnt_mutex);
+#define MAX_COUNTERMAX 100
+
+static void globalize_count(void)
+{
+	globalcount += counter;
+	counter = 0;
+	globalreserve -= countermax;
+	countermax = 0;
+}
+
+static void balance_count(void)
+{
+	countermax = globalcountmax - globalcount - globalreserve;
+	countermax /= num_online_threads();
+	if (countermax > MAX_COUNTERMAX)
+		countermax = MAX_COUNTERMAX;
+	globalreserve += countermax;
+	counter = countermax / 2;
+	if (counter > globalcount)
+		counter = globalcount;
+	globalcount -= counter;
+}
+
+int add_count(unsigned long delta)
+{
+	if (countermax - counter >= delta) {
+		counter += delta;
+		return 1;
+	}
+	spin_lock(&gblcnt_mutex);
+	globalize_count();
+	if (globalcountmax - globalcount - globalreserve < delta) {
+		spin_unlock(&gblcnt_mutex);
+		return 0;
+	}
+	globalcount += delta;
+	balance_count();
+	spin_unlock(&gblcnt_mutex);
+	return 1;
+}
+
+int sub_count(unsigned long delta)
+{
+	if (counter >= delta) {
+		counter -= delta;
+		return 1;
+	}
+	spin_lock(&gblcnt_mutex);
+	globalize_count();
+	if (globalcount < delta) {
+		spin_unlock(&gblcnt_mutex);
+		return 0;
+	}
+	globalcount -= delta;
+	balance_count();
+	spin_unlock(&gblcnt_mutex);
+	return 1;
+}
+
+unsigned long read_count(void)
+{
+	int t;
+	unsigned long sum;
+
+	spin_lock(&gblcnt_mutex);
+	sum = globalcount;
+	for_each_thread(t)
+		if (counterp[t] != NULL)
+			sum += *counterp[t];
+	spin_unlock(&gblcnt_mutex);
+	return sum;
+}
+
+void count_init(void)
+{
+}
+
+void count_register_thread(void)
+{
+	int idx = smp_thread_id();
+
+	spin_lock(&gblcnt_mutex);
+	counterp[idx] = &counter;
+	spin_unlock(&gblcnt_mutex);
+}
+
+void count_unregister_thread(int nthreadsexpected)
+{
+	int idx = smp_thread_id();
+
+	spin_lock(&gblcnt_mutex);
+	globalize_count();
+	counterp[idx] = NULL;
+	spin_unlock(&gblcnt_mutex);
+}
+
+#define NEED_REGISTER_THREAD
+#include "limtorture.h"
