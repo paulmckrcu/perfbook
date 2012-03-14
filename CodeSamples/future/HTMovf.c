a68c612aafb01a90cd15d7587719b1120c432061
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2012-03-14 14:19:15 -0700

    Add GPL header, add "C" to usage list
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- CodeSamples/future/HTMovf.c
+++ CodeSamples/future/HTMovf.c
@@ -1,3 +1,23 @@
+/*
+ * HTMovf.c: Combinatorial computation of cache-overflow probability
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Copyright (c) 2012 Paul E. McKenney, IBM Corporation.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -163,6 +183,7 @@ void usage(char *argv[])
 {
 	fprintf(stderr, "Usage: %s [ c C f p s] nsets assoc nrefs\n", argv[0]);
 	fprintf(stderr, "\tc: Check success vs. failure computation.\n");
+	fprintf(stderr, "\tC: Check success vs. failure # patterns.\n");
 	fprintf(stderr, "\tf: Compute p(failure).\n");
 	fprintf(stderr, "\tp: Compute p(failure) by most efficient means.\n");
 	fprintf(stderr, "\ts: Compute p(success).\n");
