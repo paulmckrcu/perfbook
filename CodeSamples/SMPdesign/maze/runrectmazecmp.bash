b7d148903edda767bb684fdcaf489df450ee36dd
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2012-04-15 07:58:36 -0700

    Adjust for 1000x1000 rectangular maze run.
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- CodeSamples/SMPdesign/maze/runrectmazecmp.bash
+++ CodeSamples/SMPdesign/maze/runrectmazecmp.bash
@@ -20,15 +20,15 @@ T=/tmp/runrectmazecmp.sh.$$
 trap 'rm -rf $T' 0
 mkdir $T
 
-size=500
+size=1000
 cells=$(($size*$size))
-for smallside in 1 5 10 20 50 100 200 500
+for smallside in 1 5 10 20 50 100 200 500 1000
 do
 	# Produce the mazes.
 	largeside=$(($cells/$smallside))
 	fn=${largeside}x${smallside}
 	mkdir $results/$fn > /dev/null 2>&1 || :
-	for ((i=0;i<500;i++))
+	for ((i=0;i<1000;i++))
 	do
 		./mazecmp.bash $largeside $smallside $T/maze $results/$fn
 	done
