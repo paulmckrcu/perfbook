5ac63a2fdef11f30c00589e0e6d06d5f7d71a3a4
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2016-05-31 12:34:37 -0700

    Add a crude producer/consumer memory allocator
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

---
+++ CodeSamples/datastruct/existence/procon.h
@@ -0,0 +1,94 @@
+/*
+ * procon.h: Crude producer-consumer memory allocator
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you can access it online at
+ * http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ * Copyright (c) 2016 Paul E. McKenney, IBM Corporation.
+ */
+
+#ifndef PROCON_H
+#define PROCON_H
+
+/* Individual memory block */
+struct procon_mblock {
+	struct procon_mblock *pm_next;
+};
+
+/* Producer/consumer memory pool. */
+struct procon_mpool {
+	struct procon_mblock *pm_head;
+	unsigned long pm_alloccount;
+	unsigned long pm_outcount;
+	struct procon_mblock *(*pm_alloc)(void);
+	struct procon_mblock **pm_tail
+			__attribute__((__aligned__(CACHE_LINE_SIZE)));
+	unsigned long pm_incount;
+};
+
+/* Remove a block from the pool, or get one from the alloator
+ * if the pool is low on blocks.  NULL if no memory to be had.
+ */
+static inline struct procon_mblock *procon_alloc(struct procon_mpool *pmp)
+{
+	struct procon_mblock *pmbp = pmp->pm_head;
+
+	if (pmbp == NULL || ACCESS_ONCE(pmbp->pm_next) == NULL) {
+		pmp->pm_alloccount++;
+		return pmp->pm_alloc();
+	}
+	pmp->pm_outcount++;
+	pmp->pm_head = pmbp->pm_next;
+	return pmbp;
+}
+
+void procon_free(struct procon_mpool *pmp, struct procon_mblock *pmbp)
+{
+	struct procon_mblock **nextp;
+
+	nextp = pmp->pm_tail;
+	pmp->pm_tail = &pmbp->pm_next;
+	ACCESS_ONCE(*nextp) = pmbp;
+	pmp->pm_incount++;
+}
+
+#define DEFINE_PROCON_MPOOL(type, field, alloc) \
+static inline struct procon_mblock *type##__alloc(void) \
+{ \
+	struct type *p = (alloc); \
+	\
+	if (p == NULL) \
+		return NULL; \
+	return &p->field; \
+} \
+\
+struct procon_mpool type##__procon_mpool = { \
+	.pm_alloc = type##__alloc, \
+	.pm_tail = &type##__procon_mpool.pm_head, \
+}; \
+\
+static inline struct type *type##__procon_alloc(void) \
+{ \
+	struct procon_mblock *p; \
+	\
+	p = procon_alloc(&type##__procon_mpool); \
+	return container_of(p, struct type, field); \
+} \
+\
+static inline void type##__procon_free(struct type *p) \
+{ \
+	procon_free(&type##__procon_mpool, &p->field); \
+}
+
+#endif /* #ifndef PROCON_H */
---
+++ CodeSamples/datastruct/existence/procon_test.c
@@ -0,0 +1,96 @@
+/*
+ * existence_test.c: Test existence data structures.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you can access it online at
+ * http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ * Copyright (c) 2016 Paul E. McKenney, IBM Corporation.
+ */
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <poll.h>
+#include <sys/time.h>
+#include "../../api.h"
+#define _GNU_SOURCE
+#define _LGPL_SOURCE
+#define RCU_SIGNAL
+#include <urcu.h>
+#include "existence.h"
+#include "procon.h"
+
+struct mystruct {
+	int a;
+	struct procon_mblock pm;
+	struct rcu_head rh;
+};
+
+DEFINE_PROCON_MPOOL(mystruct, pm, malloc(sizeof(struct mystruct)))
+
+void __thread (*myfree)(struct mystruct *p);
+
+struct rcu_cb_setup {
+	void (*rcs_free)(struct mystruct *p);
+	struct rcu_head rh;
+} my_rcu_cb_setup;
+
+void mystruct_rcu_cb_setup(struct rcu_head *rhp)
+{
+	struct rcu_cb_setup *rcsp;
+
+	rcsp = container_of(rhp, struct rcu_cb_setup, rh);
+	myfree = rcsp->rcs_free;
+}
+
+void mystruct_rcu_cb(struct rcu_head *rhp)
+{
+	struct mystruct *msp;
+
+	msp = container_of(rhp, struct mystruct, rh);
+	if (!myfree)
+		abort();
+	myfree(msp);
+}
+
+int main(int argc, char *argv[])
+{
+	struct call_rcu_data *crdp;
+	int i;
+	struct mystruct *msp;
+
+	rcu_register_thread();
+	run_on(0);
+	crdp = create_call_rcu_data(0, 0);
+	set_thread_call_rcu_data(crdp);
+	my_rcu_cb_setup.rcs_free = mystruct__procon_free;
+	call_rcu(&my_rcu_cb_setup.rh, mystruct_rcu_cb_setup);
+	for (i = 0; i < 100 * 1000 * 1000; i++) {
+		msp = mystruct__procon_alloc();
+		BUG_ON(!msp);
+		call_rcu(&msp->rh, mystruct_rcu_cb);
+	}
+	printf("mystruct__procon_mpool: alloc: %lu out: %lu in: %lu\n",
+	       mystruct__procon_mpool.pm_alloccount,
+	       mystruct__procon_mpool.pm_outcount,
+	       mystruct__procon_mpool.pm_incount);
+
+	poll(NULL, 0, 10);
+
+	printf("mystruct__procon_mpool: alloc: %lu out: %lu in: %lu\n",
+	       mystruct__procon_mpool.pm_alloccount,
+	       mystruct__procon_mpool.pm_outcount,
+	       mystruct__procon_mpool.pm_incount);
+
+	return 0;
+}
