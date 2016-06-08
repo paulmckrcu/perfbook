paulmckrcu/perfbook

commit 2b02f28b139857c060789ee20ec336dfa15b69ff

Makefile: Promote eps2pdf.sh to implicit rules
In the first build or a build after removing '.pdf' files by
'make neatfreak' or 'make distclean', '.pdf' figures can be built
in parallel if the rules for '.eps' -> 'pdf' and '.svn' -> 'pdf'
are given in Makefile instead of shell scripts.

This commit adds what are done in utilities/eps2pdf.sh, etc. as
implicit rules in Makefile.
As the result of the change, empty target 'embedfonts' is removed.
Now 'make -j2' works fine despite several warnings of duplicated
targets. Those warnings will be taken care of by a later patch.

Note that as of this commit, '-jN' option is not guaranteed to always
work as expected.
It is safe to use it when you specify a single target.

Using '-jN' for multiple targets such as in 'make -j2 1c 2c', some
of the targets may fail to build depending on the timing of parallel
processing, supposedly due to interference of '.aux' files used by
'bibtex'.

Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

Makefile
@@ -26,42 +26,29 @@ LATEXSOURCES = \
 	together/*.tex \
 	toolsoftrade/*.tex
 
-EPSTARGETS_OF_TEX = \
+EPSSOURCES_FROM_TEX = \
 	SMPdesign/DiningPhilosopher5.eps \
 	SMPdesign/DiningPhilosopher5TB.eps \
 	SMPdesign/DiningPhilosopher4part-b.eps \
 	SMPdesign/DiningPhilosopher5PEM.eps
 
-EPSTARGETS_OF_DOT = \
+EPSSOURCES_FROM_DOT = \
 	advsync/store15tred.eps \
 	count/sig-theft.eps
 
 EPSSOURCES = \
-	SMPdesign/*.eps \
-	advsync/*.eps \
-	appendix/questions/*.eps \
-	appendix/whymb/*.eps \
-	count/*.eps \
-	cpu/*.eps \
-	datastruct/*.eps \
-	defer/*.eps \
-	easy/*.eps \
-	future/*.eps \
-	intro/*.eps \
-	locking/*.eps \
-	$(EPSTARGETS_OF_TEX) \
-	$(EPSTARGETS_OF_DOT)
+	$(wildcard */*.eps) \
+	$(wildcard */*/*.eps) \
+	$(EPSSOURCES_FROM_TEX) \
+	$(EPSSOURCES_FROM_DOT)
+
+PDFTARGETS_OF_EPS := $(EPSSOURCES:%.eps=%.pdf)
 
 BIBSOURCES = bib/*.bib
 
-SVGSOURCES = \
-	cartoons/*.svg \
-	debugging/*.svg \
-	count/*.svg \
-	SMPdesign/*.svg \
-	defer/*.svg \
-	datastruct/*.svg \
-	rt/*.svg
+SVGSOURCES = $(wildcard */*.svg)
+
+PDFTARGETS_OF_SVG := $(SVGSOURCES:%.svg=%.pdf)
 
 default = $(PERFBOOK_DEFAULT)
 
@@ -86,10 +73,10 @@ perfbook.pdf: perfbook.bbl
 perfbook.bbl: $(BIBSOURCES) perfbook.aux
 	bibtex perfbook
 
-perfbook.aux: $(LATEXSOURCES) extraction embedfonts
+perfbook.aux: $(LATEXSOURCES) extraction
 	sh utilities/runfirstlatex.sh perfbook
 
-perfbook_flat.tex qqz.tex: perfbook.tex $(LATEXSOURCES) $(EPSSOURCES) embedfonts
+perfbook_flat.tex qqz.tex: perfbook.tex $(LATEXSOURCES) $(EPSSOURCES) $(PDFTARGETS_OF_EPS) $(PDFTARGETS_OF_SVG)
 	echo > qqz.tex
 	texexpand perfbook.tex > perfbook_flat.tex
 	sh utilities/extractqqz.sh < perfbook_flat.tex > qqz.tex
@@ -98,12 +85,6 @@ extraction: perfbook_flat.tex
 	cat perfbook_flat.tex qqz.tex | sh utilities/extractcontrib.sh > contrib.tex
 	sh utilities/extractorigpub.sh < perfbook_flat.tex > origpub.tex
 	touch extraction
-
-embedfonts: $(EPSSOURCES) $(SVGSOURCES)
-	sh utilities/fixfigfonts.sh
-	sh utilities/fixdotfonts.sh
-	sh utilities/eps2pdf.sh
-	touch embedfonts
 
 perfbook-1c.pdf: perfbook-1c.tex perfbook-1c.bbl
 	sh utilities/runlatex.sh perfbook-1c
@@ -114,7 +95,7 @@ perfbook-1c.tex: perfbook.tex
 perfbook-1c.bbl: $(BIBSOURCES) perfbook-1c.aux
 	bibtex perfbook-1c
 
-perfbook-1c.aux: $(LATEXSOURCES) extraction embedfonts
+perfbook-1c.aux: $(LATEXSOURCES) extraction
 	sh utilities/runfirstlatex.sh perfbook-1c
 
 perfbook-hb.pdf: perfbook-hb.tex perfbook-hb.bbl
@@ -126,18 +107,25 @@ perfbook-hb.tex: perfbook.tex
 perfbook-hb.bbl: $(BIBSOURCES) perfbook-hb.aux
 	bibtex perfbook-hb
 
-perfbook-hb.aux: $(LATEXSOURCES) extraction embedfonts
+perfbook-hb.aux: $(LATEXSOURCES) extraction
 	sh utilities/runfirstlatex.sh perfbook-hb
 
 # Rules related to perfbook_html are removed as of May, 2016
 
-$(EPSTARGETS_OF_TEX): %.eps: %.tex
+$(EPSSOURCES_FROM_TEX): %.eps: %.tex
 	latex -output-directory=$(shell dirname $<) $<
 	dvips -Pdownload35 -E $(patsubst %.tex,%.dvi,$<) -o $@
 	sh utilities/fixanepsfonts.sh $@
 
-$(EPSTARGETS_OF_DOT): %.eps: %.dot
+$(EPSSOURCES_FROM_DOT): %.eps: %.dot
 	dot -Tps -o $@ $<
+	sh utilities/fixanepsfonts.sh $@
+
+$(PDFTARGETS_OF_EPS): %.pdf: %.eps
+	a2ping --below --hires --bboxfrom=compute-gs $< $@
+
+$(PDFTARGETS_OF_SVG): %.pdf: %.svg
+	inkscape --export-pdf=$@ $<
 
 clean:
 	find . -name '*.aux' -o -name '*.blg' \
@@ -146,11 +134,11 @@ clean:
 	rm -f perfbook_flat.tex perfbook.out perfbook-1c.out
 	rm -f qqz.tex
 	rm -f perfbook-hb.out perfbook-1c.tex perfbook-hb.tex
-	rm -f extraction embedfonts
+	rm -f extraction
 
 distclean: clean
 	sh utilities/cleanpdf.sh
-	rm -f $(EPSTARGETS_OF_DOT) $(EPSTARGETS_OF_TEX)
+	rm -f $(EPSSOURCES_FROM_DOT) $(EPSSOURCES_FROM_TEX)
 
 touchsvg:
 	find . -name '*.svg' | xargs touch

commit 100f35a2bb870e8fa109b56b76271c7616a50bcd

build scripts: Suppress warning messages
This commit takes care of the warning messages caused by commit
2b02f28b1398 ("Makefile: Promote eps2pdf.sh to implicit rules").

It also modifies pdflatex status output so that it includes
the target .pdf file name. Status reports of warnings in .log
are commented out since they feel too verbose.

The warning message regarding missing Steel City Commit font is
also updated since the font check used to be done in eps2pdf.sh
is no longer available.

Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

Makefile
@@ -2,29 +2,8 @@ LATEXSOURCES = \
 	perfbook.tex \
 	legal.tex \
 	qqz.sty origpub.sty \
-	advsync/*.tex \
-	appendix/*.tex \
-	appendix/ack/*.tex \
-	appendix/primitives/*.tex \
-	appendix/questions/*.tex \
-	appendix/whymb/*.tex \
-	count/*.tex \
-	cpu/*.tex \
-	datastruct/*.tex \
-	debugging/*.tex \
-	easy/*.tex \
-	glossary.tex \
-	intro/*.tex \
-	memalloc/*.tex \
-	owned/*.tex \
-	defer/*.tex \
-	formal/*.tex \
-	future/*.tex \
-	howto/*.tex \
-	SMPdesign/*.tex \
-	locking/*.tex \
-	together/*.tex \
-	toolsoftrade/*.tex
+	*/*.tex \
+	*/*/*.tex
 
 EPSSOURCES_FROM_TEX = \
 	SMPdesign/DiningPhilosopher5.eps \
@@ -36,11 +15,13 @@ EPSSOURCES_FROM_DOT = \
 	advsync/store15tred.eps \
 	count/sig-theft.eps
 
-EPSSOURCES = \
+EPSSOURCES_DUP = \
 	$(wildcard */*.eps) \
 	$(wildcard */*/*.eps) \
 	$(EPSSOURCES_FROM_TEX) \
 	$(EPSSOURCES_FROM_DOT)
+
+EPSSOURCES = $(sort $(EPSSOURCES_DUP))
 
 PDFTARGETS_OF_EPS := $(EPSSOURCES:%.eps=%.pdf)
 
@@ -113,19 +94,23 @@ perfbook-hb.aux: $(LATEXSOURCES) extraction
 # Rules related to perfbook_html are removed as of May, 2016
 
 $(EPSSOURCES_FROM_TEX): %.eps: %.tex
-	latex -output-directory=$(shell dirname $<) $<
-	dvips -Pdownload35 -E $(patsubst %.tex,%.dvi,$<) -o $@
-	sh utilities/fixanepsfonts.sh $@
+	@echo "$< --> $@"
+	@latex -output-directory=$(shell dirname $<) $<
+	@dvips -Pdownload35 -E $(patsubst %.tex,%.dvi,$<) -o $@
+	@sh utilities/fixanepsfonts.sh $@
 
 $(EPSSOURCES_FROM_DOT): %.eps: %.dot
-	dot -Tps -o $@ $<
-	sh utilities/fixanepsfonts.sh $@
+	@echo "$< --> $@"
+	@dot -Tps -o $@ $<
+	@sh utilities/fixanepsfonts.sh $@
 
 $(PDFTARGETS_OF_EPS): %.pdf: %.eps
-	a2ping --below --hires --bboxfrom=compute-gs $< $@
+	@echo "$< --> $@"
+	@a2ping --below --hires --bboxfrom=compute-gs $< $@ > /dev/null 2>&1
 
 $(PDFTARGETS_OF_SVG): %.pdf: %.svg
-	inkscape --export-pdf=$@ $<
+	@echo "$< --> $@"
+	@inkscape --export-pdf=$@ $<
 
 clean:
 	find . -name '*.aux' -o -name '*.blg' \

utilities/runfirstlatex.sh
@@ -34,7 +34,7 @@ fi
 
 basename=`echo $1 | sed -e 's/\.tex$//'`
 
-echo "pdflatex 1"
+echo "pdflatex 1 for $basename.pdf"
 pdflatex $basename > /dev/null 2>&1 < /dev/null || :
 if grep -q '! Emergency stop.' $basename.log
 then

utilities/runlatex.sh
@@ -39,7 +39,7 @@ basename=`echo $1 | sed -e 's/\.tex$//'`
 iter=1
 if ! test -r $basename-first.log
 then
-	echo "pdflatex 1"
+	echo "pdflatex 1 for $basename.pdf"
 	pdflatex $basename > /dev/null 2>&1 < /dev/null || :
 	if grep -q '! Emergency stop.' $basename.log
 	then
@@ -57,12 +57,12 @@ do
 		then
 			echo "No more improvement is expected, giving up."
 			break
-		else
-			echo "Some improvements are observed, continuing."
+#		else
+#			echo "Some improvements are observed, continuing."
 		fi
 	fi
 	iter=`expr $iter + 1`
-	echo "pdflatex $iter # remaining undefined refs"
+	echo "pdflatex $iter for $basename.pdf # remaining undefined refs"
 	pdflatex $basename > /dev/null 2>&1 < /dev/null || :
 	if grep -q '! Emergency stop.' $basename.log
 	then
@@ -83,12 +83,12 @@ do
 		then
 			echo "No more improvement is expected, giving up."
 			break
-		else
-			echo "Some improvements are observed, continuing."
+#		else
+#			echo "Some improvements are observed, continuing."
 		fi
 	fi
 	iter=`expr $iter + 1`
-	echo "pdflatex $iter # label(s) may have been changed"
+	echo "pdflatex $iter for $basename.pdf # label(s) may have been changed"
 	pdflatex $basename > /dev/null 2>&1 < /dev/null || :
 	if grep -q '! Emergency stop.' $basename.log
 	then
@@ -118,7 +118,7 @@ then
 	echo "#######################################################################"
 	echo "## Steel City Comic font is not found in the resulting PDF!          ##"
 	echo "## Some speech balloons in the cartoons have been rendered awkwardly.##"
-	echo "## See item 1 in FAQ-BUILD.txt for the solution.                     ##"
+	echo "## See item 1 in FAQ.txt and FAQ-BUILD.txt to fix the font issue.    ##"
 	echo "#######################################################################"
 fi
 exit 0