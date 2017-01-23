paulmckrcu/perfbook

commit 878bf8160a5c4dc2c25c545f49e3d4f3ed6abe9e

Make date of git commit be reflected automatically
This commit adds a script to extract git commit date info to
generate autodate.tex.
If invoked on a not-clean git repository, "(m)" is appended to date
field of title.
If git status is not availalbe, current date is used instead.

Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

utilities/autodate.sh
@@ -1,0 +1,48 @@
+#!/bin/sh
+#
+# Extract git commit date info to generate autodate.tex.
+# If invoked on not-clean git repository, append "(m)" to date field
+# for title.
+# If git status is not availabe, use current date instead.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, you can access it online at
+# http://www.gnu.org/licenses/gpl-2.0.html.
+#
+# Copyright (C) Akira Yokosawa, 2017
+#
+# Authors: Akira Yokosawa <akiyks@gmail.com>
+
+export LC_TIME=C
+# check if we are in git repository
+if ! test -e .git
+then
+	date_str=`date -R`
+	modified=""
+else
+	date_str=`git show --format="%cD" | head -1`
+	# check if git status is clean
+	gitstatus=`git status --porcelain | wc -l`
+	if [ $gitstatus != "0" ]
+	then
+		modified=" (m)"
+	else
+		modified=""
+	fi
+fi
+month=`date --date="$date_str" +%B`
+year=`date --date="$date_str" +%Y`
+day=`date --date="$date_str" +%e`
+
+echo "\\date{$month $day, $year$modified}"
+echo "\\\newcommand{\\\commityear}{$year}"

.gitignore
@@ -20,6 +20,7 @@ contrib.tex
 origpub.tex
 perfbook*.out
 perfbook-*.tex
+autodate.tex
 extraction
 embedfonts
 

Makefile
@@ -7,7 +7,7 @@ LATEXSOURCES = \
 	*/*.tex \
 	*/*/*.tex
 
-LATEXGENERATED = qqz.tex contrib.tex origpub.tex
+LATEXGENERATED = autodate.tex qqz.tex contrib.tex origpub.tex
 
 ABBREVTARGETS := 1c hb mss mstx msr msn msnt 1csf
 
@@ -71,8 +71,11 @@ $(PDFTARGETS): %.pdf: %.tex %.bbl
 $(PDFTARGETS:.pdf=.bbl): %.bbl: %.aux $(BIBSOURCES)
 	bibtex $(basename $@)
 
-$(PDFTARGETS:.pdf=.aux): $(LATEXSOURCES) $(LATEXGENERATED)
+$(PDFTARGETS:.pdf=.aux): $(LATEXGENERATED) $(LATEXSOURCES)
 	sh utilities/runfirstlatex.sh $(basename $@)
+
+autodate.tex: $(LATEXSOURCES) $(BIBSOURCES) $(SVGSOURCES) $(FIGSOURCES) $(DOTSOURCES)
+	sh utilities/autodate.sh >autodate.tex
 
 perfbook_flat.tex: perfbook.tex $(LATEXSOURCES) $(PDFTARGETS_OF_EPS) $(PDFTARGETS_OF_SVG)
 	echo > qqz.tex

legal.tex
@@ -42,6 +42,6 @@ for the exact licenses.
 If you are unsure of the license for a given code fragment,
 you should assume GPLv2-only.
 
-Combined work {\textcopyright}~2005-2016 by Paul E. McKenney.
+Combined work {\textcopyright}~2005-\commityear\ by Paul E. McKenney.
 
 \IfTwoColumn{\twocolumn}{}

perfbook.tex
@@ -105,6 +105,7 @@
 	\href{mailto:paulmck@linux.vnet.ibm.com}{paulmck@linux.vnet.ibm.com} \\
 } % end author
 % \date{\ }
+\input{autodate}
 
 \setcounter{topnumber}{3}
 \renewcommand\topfraction{.75}

commit 4500e6eaf90c76f539e16c747583ef1c6f90048b

autodate.sh: Use printf command instead of echo
Using "echo" command to generate output containing "\" characters
caused a build error depending on the variant of "sh".
For portability, we should use "printf" via "env" command.

Fixes: 878bf8160a5c ("Make date of git commit be reflected automatically")
Reported-by: Trevor Woerner <twoerner@gmail.com>
Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
Tested-by: Trevor Woerner <twoerner@gmail.com>
Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

utilities/autodate.sh
@@ -35,7 +35,7 @@ else
 	gitstatus=`git status --porcelain | wc -l`
 	if [ $gitstatus != "0" ]
 	then
-		modified=" (m)"
+		modified="(m)"
 	else
 		modified=""
 	fi
@@ -44,5 +44,5 @@ month=`date --date="$date_str" +%B`
 year=`date --date="$date_str" +%Y`
 day=`date --date="$date_str" +%e`
 
-echo "\\date{$month $day, $year$modified}"
-echo "\\\newcommand{\\\commityear}{$year}"
+env printf "\\date{%s %s, %s %s}\n" $month $day $year $modified
+env printf "\\\newcommand{\\\commityear}{%s}\n" $year