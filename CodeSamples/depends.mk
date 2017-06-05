f2389cabcccab90d0898603a8fbd98055f5212a2
Akira Yokosawa <akiyks@gmail.com>
2017-06-06 00:24:15 +0900

    CodeSamples: Use $(MAKE) for recursive make
    For recursive make, "$(MAKE)" is recommended.[1]
    By this change, options such as "-j4" can be transferred to builds
    under subdirectories.
    
    Also convert the list of recursive "make"s to a "for" loop by
    defining a variable "subdirs".
    
    [1] https://www.gnu.org/software/make/manual/html_node/Recursion.html
    
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- CodeSamples/depends.mk
+++ CodeSamples/depends.mk
@@ -35,10 +35,10 @@ endif
 
 ifneq ($(top),.)
 $(top)/api.h: $(api_depend) $(api_depend_common)
-	make -C $(top) api.h
+	$(MAKE) -C $(top) api.h
 
 $(top)/Makefile.arch: $(arch_depend)
-	make -C $(top) Makefile.arch
+	$(MAKE) -C $(top) Makefile.arch
 else
 $(top)/api.h: $(api_depend) $(api_depend_common)
 $(top)/Makefile.arch: $(arch_depend)
