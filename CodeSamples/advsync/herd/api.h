931b1acfc32b76f85aff4dd0964179905196a995
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2017-07-10 06:48:41 -0700

    advsync: Add access-reordering litmus tests
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- CodeSamples/advsync/herd/api.h
+++ CodeSamples/advsync/herd/api.h
@@ -4,5 +4,8 @@
 #define READ_ONCE(x) __atomic_load_n(&(x), __ATOMIC_RELAXED)
 #define WRITE_ONCE(x, v) __atomic_store_n(&(x), (v), __ATOMIC_RELAXED)
 #define smp_mb() __atomic_thread_fence(__ATOMIC_SEQ_CST)
+#define smp_wmb() __atomic_thread_fence(__ATOMIC_RELEASE) /* outside std. */
+#define smp_load_acquire(x) __atomic_load_n(x, __ATOMIC_ACQUIRE)
+#define smp_store_release(x, v) __atomic_store_n(x, v, __ATOMIC_RELEASE)
 #endif
 #endif
