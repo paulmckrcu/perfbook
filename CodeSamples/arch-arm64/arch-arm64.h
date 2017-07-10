a339a8e1eb7406d189a8294478f8caedefb5759c
Akira Yokosawa <akiyks@gmail.com>
2017-07-10 23:46:32 +0900

    CodeSamples: Add arm64 support
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

---
+++ CodeSamples/arch-arm64/arch-arm64.h
@@ -0,0 +1,61 @@
+/*
+ * arch-arm64.h: Expose ARMv8_64 atomic instructions.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; but version 2 of the License only due
+ * to code included from the Linux kernel.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you can access it online at
+ * http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ * Copyright (c) 2010 Paul E. McKenney, IBM.
+ *
+ * Much code taken from the Linux kernel.  For such code, the option
+ * to redistribute under later versions of GPL might not be available.
+ */
+
+/*
+ * Machine parameters.
+ */
+
+#define CONFIG_SMP
+#define CONFIG_ARM64
+
+#define CACHE_LINE_SIZE 128
+#define ____cacheline_internodealigned_in_smp \
+	__attribute__((__aligned__(1 << 7)))
+
+/* The gcc primitives imply full memory barriers. */
+#define smp_mb__before_atomic_dec()	do { } while (0)
+#define smp_mb__after_atomic_dec()	do { } while (0)
+#define smp_mb__before_atomic_inc()	do { } while (0)
+#define smp_mb__after_atomic_inc()	do { } while (0)
+
+/* __sync_synchronize() is broken before gcc 4.4.1 on many ARM systems. */
+#define smp_mb()  __asm__ __volatile__("dmb ish" : : : "memory")
+
+
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * Generate 64-bit timestamp.
+ */
+static __inline__ unsigned long long get_timestamp(void)
+{
+	unsigned long long thetime;
+	struct timespec tv;
+
+	if (clock_gettime(CLOCK_MONOTONIC_RAW, &tv) != 0)
+		return 0;
+	thetime = ((unsigned long long)tv.tv_sec) * 1000000000ULL +
+		  ((unsigned long long)tv.tv_nsec);
+	return thetime;
+}
--- CodeSamples/depends.mk
+++ CodeSamples/depends.mk
@@ -10,6 +10,8 @@ else ifeq ($(arch),ppc64)
 target := ppc64
 else ifeq ($(arch),ppc64le)
 target := ppc64
+else ifeq ($(arch),aarch64)
+target := arm64
 else ifneq (,$(findstring arm,$(arch)))
 target := arm
 else
@@ -27,6 +29,9 @@ arch_depend := $(top)/arch-ppc64/Makefile.arch
 else ifeq ($(target),arm)
 api_depend := $(top)/arch-arm/arch-arm.h
 arch_depend := $(top)/arch-arm/Makefile.arch
+else ifeq ($(target),arm64)
+api_depend := $(top)/arch-arm64/arch-arm64.h
+arch_depend := $(top)/arch-arm64/Makefile.arch
 else
 api_depend :=
 arch_depend :=
