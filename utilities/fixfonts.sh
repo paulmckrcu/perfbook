ec7c19ec81c2a11e626a86174faa4af14681472b
Akira Yokosawa <akiyks@gmail.com>
2018-01-27 12:47:49 +0900

    Fix pattern order in fixfonts.sh
    Longer matching patters should come first.
    Also add several patterns for Helvetica Narrow fonts.
    
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- utilities/fixfonts.sh
+++ utilities/fixfonts.sh
@@ -16,17 +16,33 @@
 # http://www.gnu.org/licenses/gpl-2.0.html.
 #
 # Copyright (c) 2011 Paul E. McKenney, IBM Corporation.
+# Copyright (c) 2018 Akira Yokosawa
 
-sed	-e 's+Times-Roman+NimbusSanL-Regu+g' \
+sed	-e 's+Times-Roman-BoldItalic+NimbusSanL-BoldItal+g' \
+	-e 's+Times-Roman-Italic+NimbusSanL-ReguItal+g' \
+	-e 's+Times-Roman-Bold+NimbusSanL-Bold+g' \
+	-e 's+Times-Roman+NimbusSanL-Regu+g' \
 	-e 's+Times+NimbusSanL-Regu+g' \
 	-e 's+Helvetica-BoldOblique+NimbusSanL-BoldItal+g' \
 	-e 's+Helvetica-Oblique+NimbusSanL-ReguItal+g' \
-	-e 's+Helvetica-Bold+NimbusSanL-Bold+g' \
 	-e 's+Helvetica-Bold-iso+NimbusSanL-Bold+g' \
-	-e 's+Helvetica+NimbusSanL-Regu+g' \
+	-e 's+Helvetica-Bold+NimbusSanL-Bold+g' \
+	-e 's+Helvetica-Narrow-BoldOblique-iso+NimbusSanL-BoldCondItal+g' \
+	-e 's+Helvetica-Narrow-BoldOblique+NimbusSanL-BoldCondItal+g' \
+	-e 's+Helvetica-Narrow-Oblique-iso+NimbusSanL-ReguCondItal+g' \
+	-e 's+Helvetica-Narrow-Oblique+NimbusSanL-ReguCondItal+g' \
+	-e 's+Helvetica-Narrow-Bold-iso+NimbusSanL-BoldCond+g' \
+	-e 's+Helvetica-Narrow-Bold+NimbusSanL-BoldCond+g' \
+	-e 's+Helvetica-Narrow-iso+NimbusSanL-ReguCond+g' \
+	-e 's+Helvetica-Narrow+NimbusSanL-ReguCond+g' \
 	-e 's+Helvetica-iso+NimbusSanL-Regu+g' \
+	-e 's+Helvetica+NimbusSanL-Regu+g' \
 	-e 's+Symbol+StandardSymL+g' \
-	-e 's+Courier+NimbusMonL-Regu+g' \
-	-e 's+Courier-Bold+NimbusMonL-Bold+g' \
+	-e 's+Courier-BoldOblique+NimbusMonL-BoldObli+g' \
 	-e 's+Courier-Oblique+NimbusMonL-ReguObli+g' \
-	-e 's+Courier-BoldOblique+NimbusMonL-BoldObli+g'
+	-e 's+Courier-Bold+NimbusMonL-Bold+g' \
+	-e 's+Courier+NimbusMonL-Regu+g' \
+	-e 's+NimbusSanL-Regu-Italic+NimbusSanL-ReguItal+g' \
+	-e 's+NimbusSanL-Regu-BoldItalic+NimbusSanL-BoldItal+g' \
+	-e 's+NimbusSanL-Regu-Bold+NimbusSanL-Bold+g' \
+	-e 's+StardardSymL-+Symbol-+g'
