5fb01faee1d82760507fb33c80dd78bcb824cafb
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-04-09 15:11:00 -0700

    Initial version of lock wakeup-latency benchmark
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

---
+++ CodeSamples/locking/lockwakelatency.c
@@ -0,0 +1,171 @@
+/*
+ * lockwakelatency.c: Measure time required to unlock-wake sleeping thread
+ *
+ * Usage:
+ * 	./lockwakelatency <nlockers> [ <duration in seconds> ]
+ * 		Run a wakeup-latency test with the specified number of
+ *		threads to be awakened.  Defaults to a one-second test.
+ *
+ * The command "./lockwakelatency 2" produces output as follows:
+ *
+ * n_rounds: @@@  nlockers: 2  wake latency: @@@  test duration: 1  ns/wakeup: @@@
+ *
+ * The "n_rounds" is the number of rounds of lock wakeup testing, the
+ * "nlockers" is the number of locking threads, the "wake latency" is
+ * the total time spent awakening, "test duration" is the test's wallclock
+ * time, and ns/wakeup is the number of nanoseconds consumed by each
+ * individual wakeup, where each round will have nlockers wakeups.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you can access it online at
+ * http://www.gnu.org/licenses/gpl-2.0.html.
+ *
+ * Copyright (c) 2018 Paul E. McKenney, IBM Corporation.
+ */
+
+#include <stdio.h>
+#include <stddef.h>
+#include <time.h>
+
+#include "../api.h"
+
+/* CLOCK_MONOTONIC_RAW prefered, but the older CLOCK_MONOTONIC will do. */
+#ifdef CLOCK_MONOTONIC_RAW
+#define TEST_CLOCK CLOCK_MONOTONIC_RAW
+#else /* #ifdef CLOCK_MONOTONIC_RAW */
+#define TEST_CLOCK CLOCK_MONOTONIC
+#endif /* #else #ifdef CLOCK_MONOTONIC_RAW */
+
+/* Get current time in free-running nanoseconds. */
+unsigned long long current_time(void)
+{
+	struct timespec t;
+
+	if (clock_gettime(TEST_CLOCK, &t) != 0)
+		abort();
+	return (unsigned long long)t.tv_sec * 1000000000ULL +
+	       (unsigned long long)t.tv_nsec;
+}
+
+/*
+ * Test variables.
+ */
+
+int duration = 1;
+int n_rounds;
+atomic_t acqctr;
+atomic_t relctr;
+atomic_t donectr;
+
+#define GOFLAG_INIT 0
+#define GOFLAG_RUN  1
+#define GOFLAG_STOP 2
+
+int goflag __attribute__((__aligned__(CACHE_LINE_SIZE))) = GOFLAG_RUN;
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+/*
+ * Locking wakeup latency test.
+ */
+void *lock_wake_latency_test(void *arg)
+{
+	while (READ_ONCE(goflag) == GOFLAG_RUN) {
+		atomic_dec(&acqctr);
+		if (pthread_mutex_lock(&lock) != 0) {
+			perror("lock_wake_latency_test:pthread_mutex_lock");
+			abort();
+		}
+		if (pthread_mutex_unlock(&lock) != 0) {
+			perror("lock_wake_latency_test:pthread_mutex_unlock");
+			abort();
+		}
+		atomic_dec(&relctr);
+	}
+	atomic_dec(&donectr);
+	return (NULL);
+}
+
+void perftest(int nlockers)
+{
+	intptr_t i;
+	unsigned long long t;
+	unsigned long long tdelta;
+	unsigned long long tstart;
+	unsigned long long ttot;
+
+	atomic_set(&acqctr, nlockers);
+	atomic_set(&relctr, nlockers);
+	atomic_set(&donectr, nlockers);
+	for (i = 0; i < nlockers; i++)
+		create_thread(lock_wake_latency_test, (void *)i);
+	while (atomic_read(&acqctr) > 0)
+		poll(NULL, 0, 1);
+	smp_mb();
+	WRITE_ONCE(goflag, GOFLAG_RUN);
+	tstart = current_time();
+	do {
+		if (pthread_mutex_lock(&lock) != 0) {
+			perror("lock_wake_latency_test:pthread_mutex_lock");
+			abort();
+		}
+		smp_mb();
+		while (atomic_read(&acqctr) > 0)
+			poll(NULL, 0, 1);
+		smp_mb();
+		atomic_set(&acqctr, nlockers);
+		atomic_set(&relctr, nlockers);
+		poll(NULL, 0, 10);
+		t = current_time();
+		smp_mb();
+		if (pthread_mutex_unlock(&lock) != 0) {
+			perror("lock_wake_latency_test:pthread_mutex_unlock");
+			abort();
+		}
+		while (atomic_read(&relctr) > 0)
+			continue;
+		smp_mb();
+		tdelta = current_time() - t;
+		ttot += tdelta;
+/*&&&&*/	printf("delta = %lld ns\n", tdelta);
+		n_rounds++;
+	} while (tstart + duration * 1000000000ULL > t);
+	WRITE_ONCE(goflag, GOFLAG_STOP);
+	while (atomic_read(&donectr) > 0)
+		poll(NULL, 0, 1);
+	printf("n_rounds: %d  nlockers: %d  wake latency: %g  test duration: %d  ns/wakeup %lld\n",
+	       n_rounds, nlockers, (double)ttot / 1000000000., duration, ttot / n_rounds / nlockers);
+	exit(0);
+}
+
+
+/*
+ * Mainprogram.
+ */
+
+int main(int argc, char *argv[])
+{
+	int nlockers = 0;
+
+	smp_init();
+
+	if (argc > 1) {
+		nlockers = strtoul(argv[1], NULL, 0);
+		if (argc == 3)
+			duration = strtoul(argv[2], NULL, 0);
+		if (argc <= 3)
+			perftest(nlockers);
+	}
+	fprintf(stderr, "Usage: %s nlockers [ duration (s) ]\n", argv[0]);
+	exit(-1);
+}
