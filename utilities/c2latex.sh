e3191997557e0d33b862e267fb4d5971e879ca07
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2018-07-18 11:40:35 -0700

    utilities: Provide scripts instead of broken symlinks
    Reported-by: Akira Yokosawa <akiyks@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- utilities/c2latex.sh
+++ utilities/c2latex.sh
@@ -1,1 +1,23 @@
-/home/paulmck/bin/c2latex.sh
+#!/bin/sh
+#
+# Numbers a code fragment and converts tabs to spaces.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, you can access it online at
+# http://www.gnu.org/licenses/gpl-2.0.html.
+#
+# Copyright (C) IBM Corporation, 2007
+#
+# Authors: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
+
+awk '{printf "%2d %s\n", NR, $0}' | sed -e 's/	/  /g' | sed -e 's/ *$//'
--- utilities/extractClatex.sh
+++ utilities/extractClatex.sh
@@ -1,1 +1,58 @@
-/home/paulmck/bin/extractClatex.sh
+#!/bin/bash
+#
+# Extracts marked code fragments and places them into files specified by
+# the markings.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Copyright (C) IBM Corporation, 2007
+#
+# Authors: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
+
+if test "$1" = ""
+then
+	cat
+else
+	cat "$@"
+fi |
+awk	'
+BEGIN	{
+	state = "scanning";
+	}
+
+	{
+	if (state == "scanning") {
+		if ($1 == "%CODE%") {
+			state = "find-verbatim";
+			filename = $2;
+			if (fns[filename] == 1) {
+				print "echo Warning: duplicate filename: " $2;
+			}
+			fns[filename] = 1;
+		}
+	} else if (state == "find-verbatim") {
+		if ($1 == "\\begin{verbatim}") {
+			state = "copy-code";
+			print "cat > " filename " << '"'"'@@@EOF@@@'"'"'";
+		}
+	} else if (state == "copy-code") {
+		if ($1 != "\\end{verbatim}") {
+			print substr($0, 5);
+		} else {
+			print "@@@EOF@@@";
+			state = "scanning";
+		}
+	}
+	}' | sh
