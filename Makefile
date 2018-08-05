paulmckrcu/perfbook

commit d9a36224084b2e4ba55797eff0c37de528b59d6b

Add dependencies of perfbook_flat.tex to CodeSamples/.../*.fcv files
Instead of generating CodeSamples/snippets.mk and calling it from
main Makefile, by generating CodeSamples/snippets.d which contains
just dependencies and including it in the Makefile,
we can tell "make" the dependency of perfbook_flat.tex to those
*.fcv files.

By doing this change, .fcv files can be nicely handled in parallel
make (make -jN).

Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

Makefile
@@ -83,7 +83,14 @@ endif
 endif
 
 SOURCES_OF_SNIPPET := $(shell grep -r -l -F '\begin{snippet}' CodeSamples)
-GEN_SNIPPET_MK = utilities/gen_snippet_mk.pl utilities/gen_snippet_mk.sh
+GEN_SNIPPET_D  = utilities/gen_snippet_d.pl utilities/gen_snippet_d.sh
+
+ifeq ($(MAKECMDGOALS),clean)
+else ifeq ($(MAKECMDGOALS),distclean)
+else ifeq ($(MAKECMDGOALS),neatfreak)
+else
+-include CodeSamples/snippets.d
+endif
 
 default = $(PERFBOOK_DEFAULT)
 
@@ -118,14 +125,13 @@ $(PDFTARGETS:.pdf=.aux): $(LATEXGENERATED) $(LATEXSOURCES)
 autodate.tex: perfbook.tex $(LATEXSOURCES) $(BIBSOURCES) $(SVGSOURCES) $(FIGSOURCES) $(DOTSOURCES) $(EPSORIGIN) $(SOURCES_OF_SNIPPET) utilities/fcvextract.pl
 	sh utilities/autodate.sh >autodate.tex
 
-perfbook_flat.tex: autodate.tex $(PDFTARGETS_OF_EPS) $(TARGETS_OF_SVG) CodeSamples/snippets.mk
+perfbook_flat.tex: autodate.tex $(PDFTARGETS_OF_EPS) $(TARGETS_OF_SVG) $(FCVSNIPPETS)
 ifndef LATEXPAND
 	$(error --> $@: latexpand not found. Please install it)
 endif
 	echo > qqz.tex
 	echo > contrib.tex
 	echo > origpub.tex
-	$(MAKE) -C CodeSamples -f snippets.mk
 	latexpand --empty-comments perfbook.tex 1> $@ 2> /dev/null
 
 qqz.tex: perfbook_flat.tex
@@ -252,8 +258,12 @@ endif
 	@inkscape --export-dpi=200 --export-png=$@ $<i > /dev/null 2>&1
 	@rm -f $<i
 
-CodeSamples/snippets.mk: $(SOURCES_OF_SNIPPET) $(GEN_SNIPPET_MK)
-	sh ./utilities/gen_snippet_mk.sh
+CodeSamples/snippets.d: $(SOURCES_OF_SNIPPET) $(GEN_SNIPPET_D)
+	sh ./utilities/gen_snippet_d.sh
+
+$(FCVSNIPPETS):
+	@echo "$< --> $@"
+	@utilities/fcvextract.pl $< $(subst @,:,$(basename $(notdir $@))) > $@
 
 help:
 	@echo "Official targets (Latin Modern Typewriter for monospace font):"
@@ -290,7 +300,7 @@ clean:
 	rm -f $(LATEXGENERATED)
 	rm -f $(SVG_LARGE_BITMAP:%.svg=%.pdf) $(PNGTARGETS_OF_SVG)
 	rm -f extraction
-	rm -f CodeSamples/snippets.mk
+	rm -f CodeSamples/snippets.mk CodeSamples/snippets.d
 
 distclean: clean
 	sh utilities/cleanpdf.sh

.gitignore
@@ -27,7 +27,7 @@ perfbook-*.tex
 autodate.tex
 extraction
 embedfonts
-CodeSamples/snippets.mk
+CodeSamples/snippets.d
 
 RCS
 

utilities/gen_snippet_d.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 # SPDX-License-Identifier: GPL-2.0-or-later
 #
-# Generate CodeSamples/snippets.mk
+# Generate CodeSamples/snippets.d
 #
-# Note: Output file name is specified in main Makefile.
+# Note: Output file name is specified in gen_snippet_d.sh
 #
 # Copyright (C) Akira Yokosawa, 2018
 #
@@ -22,7 +22,7 @@ $snippet_key = '\\begin\{snippet\}' ;
 chomp @fcvsources ;
 
 print "# Do not edit!\n" ;
-print "# Generated by utilities/gen_snippet_mk.pl.\n\n" ;
+print "# Generated by utilities/gen_snippet_d.pl.\n\n" ;
 print "FCVSNIPPETS = " ;
 foreach $source (@fcvsources) {
     my @snippet_commands1 ;
@@ -36,12 +36,11 @@ foreach $source (@fcvsources) {
 	$snippet =~ /labelbase=.*:(.+:[^,\]]+)[,\]]/ ;
 	$_ = $1;
 	s/:/@/g ;
-	print "\\\n\t$subdir/$_.fcv ";
+	print "\\\n\tCodeSamples/$subdir/$_.fcv ";
     }
 }
-print "\n\nEXTRACT = ../utilities/fcvextract.pl\n" ;
-print "\n\.PHONY: all clean\n\n" ;
-print "all: \$\(FCVSNIPPETS\)\n\n" ;
+
+print "\n\nEXTRACT = utilities/fcvextract.pl\n\n" ;
 
 foreach $source (@fcvsources) {
     my @snippet_commands2 ;
@@ -51,9 +50,8 @@ foreach $source (@fcvsources) {
     @snippet_commands2 = `grep -F $snippet_key $source` ;
     chomp @snippet_commands2 ;
     $src_under_sub = $source ;
-    $source =~ m!.*/([^/]+)/[^/]+! ;
+    $source =~ m!(.*/[^/]+)/[^/]+! ;
     $subdir = $1 ;
-    $src_under_sub =~ s/CodeSamples\/// ;
 #    print @snippet_commands ;
     foreach $snippet (@snippet_commands2) {
 	$snippet =~ /labelbase=.*:(.+:[^,\]]+)[,\]]/ ;
@@ -65,8 +63,3 @@ foreach $source (@fcvsources) {
 	print "$subdir/$_.fcv: $src_under_sub \$\(EXTRACT\)\n";
     }
 }
-
-print "\n\$\(FCVSNIPPETS\):\n" ;
-print "\t\@echo \"\$< --> \$\@\"\n" ;
-print "\t\@../utilities/fcvextract.pl \$< \$\(subst \@,:,\$\(basename \$\(notdir \$\@\)\)\) > \$\@\n" ;
-print "\nclean:\n\trm -f \$\(FCVSNIPPETS\)\n" ;

utilities/gen_snippet_d.sh
@@ -1,0 +1,8 @@
+#!/bin/sh
+./utilities/gen_snippet_d.pl > CodeSamples/snippets.d
+rc=$?
+if [ $rc != 0 ]
+then
+	rm -f CodeSamples/snippets.d
+	exit 1
+fi

utilities/gen_snippet_mk.sh
@@ -1,8 +1,0 @@
-#!/bin/sh
-./utilities/gen_snippet_mk.pl > CodeSamples/snippets.mk
-rc=$?
-if [ $rc != 0 ]
-then
-	rm -f CodeSamples/snippets.mk
-	exit 1
-fi

commit 78141e64d4629b29643634fc1d334233f108f656

Makefile: Fix "make" behavior
"make" treats the first target in a Makefile as a default.
Inclusion of CodeSamples/snippets.d added in commit d9a36224084b
("Add dependencies of perfbook_flat.tex to CodeSamples/.../*.fcv
files") conflicts this convention.

Fix this by moving the include statement.

Fixes: d9a36224084b
Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
Acked-by: Akira Yokosawa <akiyks@gmail.com>
[akiyks: Amended commit log]
Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

Makefile
@@ -85,13 +85,6 @@ endif
 SOURCES_OF_SNIPPET := $(shell grep -r -l -F '\begin{snippet}' CodeSamples)
 GEN_SNIPPET_D  = utilities/gen_snippet_d.pl utilities/gen_snippet_d.sh
 
-ifeq ($(MAKECMDGOALS),clean)
-else ifeq ($(MAKECMDGOALS),distclean)
-else ifeq ($(MAKECMDGOALS),neatfreak)
-else
--include CodeSamples/snippets.d
-endif
-
 default = $(PERFBOOK_DEFAULT)
 
 ifeq ($(default),)
@@ -102,6 +95,13 @@ endif
 
 .PHONY: all touchsvg clean distclean neatfreak 2c ls-unused $(ABBREVTARGETS) mslm perfbook-mslm.pdf mslmmsg help
 all: $(targ)
+
+ifeq ($(MAKECMDGOALS),clean)
+else ifeq ($(MAKECMDGOALS),distclean)
+else ifeq ($(MAKECMDGOALS),neatfreak)
+else
+-include CodeSamples/snippets.d
+endif
 
 2c: perfbook.pdf
 