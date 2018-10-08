e84f6a72f1646fe16d44ec58e99efc880864de8b
Akira Yokosawa <akiyks@gmail.com>
2018-10-09 01:45:32 +0900

    count: Employ new scheme for snippet of count_lim_sig
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

--- CodeSamples/count/count_lim_sig.c
+++ CodeSamples/count/count_lim_sig.c
@@ -22,77 +22,81 @@
 #include "../api.h"
 #include <signal.h>
 
-#define THEFT_IDLE	0
-#define THEFT_REQ	1
-#define	THEFT_ACK	2
-#define THEFT_READY	3
+//\begin{snippet}[labelbase=ln:count:count_lim_sig:data,commandchars=\\\@\$]
+#define THEFT_IDLE   0					//\lnlbl{value:b}
+#define THEFT_REQ    1
+#define	THEFT_ACK    2
+#define THEFT_READY  3
 
 int __thread theft = THEFT_IDLE;
-int __thread counting = 0;
-unsigned long __thread counter = 0;
+int __thread counting = 0;				//\lnlbl{value:e}
+unsigned long __thread counter = 0;			//\lnlbl{var:b}
 unsigned long __thread countermax = 0;
 unsigned long globalcountmax = 10000;
 unsigned long globalcount = 0;
 unsigned long globalreserve = 0;
 unsigned long *counterp[NR_THREADS] = { NULL };
-unsigned long *countermaxp[NR_THREADS] = { NULL };
-int *theftp[NR_THREADS] = { NULL };
+unsigned long *countermaxp[NR_THREADS] = { NULL };	//\lnlbl{maxp}
+int *theftp[NR_THREADS] = { NULL };			//\lnlbl{theftp}
 DEFINE_SPINLOCK(gblcnt_mutex);
-#define MAX_COUNTERMAX 100
+#define MAX_COUNTERMAX 100				//\lnlbl{var:e}
+//\end{snippet}
 
-static void globalize_count(void)
+//\begin{snippet}[labelbase=ln:count:count_lim_sig:migration,commandchars=\\\@\$]
+static void globalize_count(void)		//\lnlbl{globalize:b}
 {
 	globalcount += counter;
 	counter = 0;
 	globalreserve -= countermax;
 	countermax = 0;
-}
+}						//\lnlbl{globalize:e}
 
-static void flush_local_count_sig(int unused)
+static void flush_local_count_sig(int unused)	//\lnlbl{flush_sig:b}
 {
-	if (READ_ONCE(theft) != THEFT_REQ)
-		return;
-	smp_mb();
-	WRITE_ONCE(theft, THEFT_ACK);
-	if (!counting) {
-		WRITE_ONCE(theft, THEFT_READY);
+	if (READ_ONCE(theft) != THEFT_REQ)	//\lnlbl{flush_sig:check:REQ}
+		return;				//\lnlbl{flush_sig:return:n}
+	smp_mb();				//\lnlbl{flush_sig:mb:1}
+	WRITE_ONCE(theft, THEFT_ACK);		//\lnlbl{flush_sig:set:ACK}
+	if (!counting) {			//\lnlbl{flush_sig:check:fast}
+		WRITE_ONCE(theft, THEFT_READY);	//\lnlbl{flush_sig:set:READY}
 	}
 	smp_mb();
-}
+}						//\lnlbl{flush_sig:e}
 
-static void flush_local_count(void)
+static void flush_local_count(void)			//\lnlbl{flush:b}
 {
 	int t;
 	thread_id_t tid;
 
-	for_each_tid(t, tid)
-		if (theftp[t] != NULL) {
-			if (*countermaxp[t] == 0) {
-				WRITE_ONCE(*theftp[t], THEFT_READY);
-				continue;
+	for_each_tid(t, tid)				//\lnlbl{flush:loop:b}
+		if (theftp[t] != NULL) {		//\lnlbl{flush:skip}
+			if (*countermaxp[t] == 0) {	//\lnlbl{flush:checkmax}
+				WRITE_ONCE(*theftp[t], THEFT_READY);//\lnlbl{flush:READY}
+				continue;		//\lnlbl{flush:next}
 			}
-			WRITE_ONCE(*theftp[t], THEFT_REQ);
-			pthread_kill(tid, SIGUSR1);
-		}
-	for_each_tid(t, tid) {
-		if (theftp[t] == NULL)
-			continue;
-		while (READ_ONCE(*theftp[t]) != THEFT_READY) {
-			poll(NULL, 0, 1);
-			if (READ_ONCE(*theftp[t]) == THEFT_REQ)
-				pthread_kill(tid, SIGUSR1);
-		}
-		globalcount += *counterp[t];
+			WRITE_ONCE(*theftp[t], THEFT_REQ);//\lnlbl{flush:REQ}
+			pthread_kill(tid, SIGUSR1);	//\lnlbl{flush:signal}
+		}					//\lnlbl{flush:loop:e}
+	for_each_tid(t, tid) {				//\lnlbl{flush:loop2:b}
+		if (theftp[t] == NULL)			//\lnlbl{flush:skip:nonexist}
+			continue;			//\lnlbl{flush:next2}
+		while (READ_ONCE(*theftp[t]) != THEFT_READY) {//\lnlbl{flush:loop3:b}
+			poll(NULL, 0, 1);		//\lnlbl{flush:block}
+			if (READ_ONCE(*theftp[t]) == THEFT_REQ)//\lnlbl{flush:check:REQ}
+				pthread_kill(tid, SIGUSR1);//\lnlbl{flush:signal2}
+		}					//\lnlbl{flush:loop3:e}
+		globalcount += *counterp[t];		//\lnlbl{flush:thiev:b}
 		*counterp[t] = 0;
 		globalreserve -= *countermaxp[t];
-		*countermaxp[t] = 0;
-		WRITE_ONCE(*theftp[t], THEFT_IDLE);
-	}
-}
+		*countermaxp[t] = 0;			//\lnlbl{flush:thiev:e}
+		WRITE_ONCE(*theftp[t], THEFT_IDLE);	//\lnlbl{flush:IDLE}
+	}						//\lnlbl{flush:loop2:e}
+}							//\lnlbl{flush:e}
 
-static void balance_count(void)
+static void balance_count(void)				//\lnlbl{balance:b}
 {
-	countermax = globalcountmax - globalcount - globalreserve;
+	countermax = globalcountmax - globalcount -
+	             globalreserve;
 	countermax /= num_online_threads();
 	if (countermax > MAX_COUNTERMAX)
 		countermax = MAX_COUNTERMAX;
@@ -101,42 +105,49 @@ static void balance_count(void)
 	if (counter > globalcount)
 		counter = globalcount;
 	globalcount -= counter;
-}
+}							//\lnlbl{balance:e}
+//\end{snippet}
 
-int add_count(unsigned long delta)
+//\begin{snippet}[labelbase=ln:count:count_lim_sig:add,commandchars=\\\@\$]
+int add_count(unsigned long delta)			//\lnlbl{b}
 {
 	int fastpath = 0;
 
-	counting = 1;
-	barrier();
-	if (countermax - counter >= delta && READ_ONCE(theft) <= THEFT_REQ) {
-		counter += delta;
-		fastpath = 1;
+	counting = 1;					//\lnlbl{fast:b}
+	barrier();					//\lnlbl{barrier:1}
+	if (countermax - counter >= delta &&		//\lnlbl{check:b}
+	    READ_ONCE(theft) <= THEFT_REQ) {		//\lnlbl{check:e}
+		counter += delta;			//\lnlbl{add:f}
+		fastpath = 1;				//\lnlbl{fasttaken}
 	}
-	barrier();
-	counting = 0;
-	barrier();
-	if (READ_ONCE(theft) == THEFT_ACK) {
-		smp_mb();
-		WRITE_ONCE(theft, THEFT_READY);
+	barrier();					//\lnlbl{barrier:2}
+	counting = 0;					//\lnlbl{clearcnt}
+	barrier();					//\lnlbl{barrier:3}
+	if (READ_ONCE(theft) == THEFT_ACK) {		//\lnlbl{check:ACK}
+		smp_mb();				//\lnlbl{mb}
+		WRITE_ONCE(theft, THEFT_READY);		//\lnlbl{READY}
 	}
 	if (fastpath)
-		return 1;
-	spin_lock(&gblcnt_mutex);
+		return 1;				//\lnlbl{return:fs}
+	spin_lock(&gblcnt_mutex);			//\lnlbl{acquire}
 	globalize_count();
-	if (globalcountmax - globalcount - globalreserve < delta) {
+	if (globalcountmax - globalcount -
+	    globalreserve < delta) {
 		flush_local_count();
-		if (globalcountmax - globalcount - globalreserve < delta) {
-			spin_unlock(&gblcnt_mutex);
-			return 0;
+		if (globalcountmax - globalcount -
+		    globalreserve < delta) {
+			spin_unlock(&gblcnt_mutex);	//\lnlbl{release:f}
+			return 0;			//\lnlbl{return:sf}
 		}
 	}
 	globalcount += delta;
 	balance_count();
 	spin_unlock(&gblcnt_mutex);
-	return 1;
-}
+	return 1;					//\lnlbl{return:ss}
+}							//\lnlbl{e}
+//\end{snippet}
 
+//\begin{snippet}[labelbase=ln:count:count_lim_sig:sub,commandchars=\\\@\$]
 int sub_count(unsigned long delta)
 {
 	int fastpath = 0;
@@ -170,7 +181,9 @@ int sub_count(unsigned long delta)
 	spin_unlock(&gblcnt_mutex);
 	return 1;
 }
+//\end{snippet}
 
+//\begin{snippet}[labelbase=ln:count:count_lim_sig:read,commandchars=\\\@\$]
 unsigned long read_count(void)
 {
 	int t;
@@ -184,8 +197,10 @@ unsigned long read_count(void)
 	spin_unlock(&gblcnt_mutex);
 	return sum;
 }
+//\end{snippet}
 
-void count_init(void)
+//\begin{snippet}[labelbase=ln:count:count_lim_sig:initialization,commandchars=\\\@\$]
+void count_init(void)					//\lnlbl{init:b}
 {
 	struct sigaction sa;
 
@@ -196,7 +211,7 @@ void count_init(void)
 		perror("sigaction");
 		exit(EXIT_FAILURE);
 	}
-}
+}							//\lnlbl{init:e}
 
 void count_register_thread(void)
 {
@@ -220,6 +235,7 @@ void count_unregister_thread(int nthreadsexpected)
 	theftp[idx] = NULL;
 	spin_unlock(&gblcnt_mutex);
 }
+//\end{snippet}
 
 void count_cleanup(void)
 {
