31cac5d6379cc4eda09628a5c148b5f3eed9668a
Akira Yokosawa <akiyks@gmail.com>
2018-11-11 23:30:04 +0900

    styleguide: Update code snippet section
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>
    [ paulmck: Addressed a few grammar and usage nits. ]

---
+++ appendix/styleguide/samplecodesnippet.c
@@ -0,0 +1,11 @@
+//\begin{snippet}[labelbase=ln:base1,commandchars=\$\[\]]
+/*
+ * Sample Code Snippet
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("Hello world!\n");	//\lnlbl{printf}
+	return 0;			//\lnlbl{return}
+}
+//\end{snippet}
