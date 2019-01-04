00c2097dc6140d9e89aa0ca4cb21f0212b4cfb34
Paul E. McKenney <paulmck@linux.ibm.com>
2019-01-04 11:34:21 -0800

    datastruct/hash: Debug checks prior to hashtab_add() revamp
    The goal is to make hashtab_add() refrain from rehashing and to make
    the coupling of the locking and the actual linked-list manipulation
    be correct by construction.  But for that to be the case, it is necessary
    for the values computed at hashtab_lock_mod() time to actually match
    those computed at hashtab_add() time.
    
    This commit adds debug checks to check this matching.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

--- CodeSamples/datastruct/hash/hash_resize.c
+++ CodeSamples/datastruct/hash/hash_resize.c
@@ -251,6 +251,7 @@ hashtab_lookup(struct hashtab *htp_master, void *key)
 void hashtab_add(struct hashtab *htp_master,		//\lnlbl{add:b}
                  struct ht_elem *htep, struct ht_lock_state *lsp)
 {
+	int new = !!lsp->hbp[1];
 	long b;
 	int i;
 	struct ht *htp;
@@ -260,7 +261,10 @@ void hashtab_add(struct hashtab *htp_master,		//\lnlbl{add:b}
 	htp = rcu_dereference(htp_master->ht_cur);	//\lnlbl{add:get_curtbl}
 	k = htp->ht_getkey(htep);
 	htep->hte_hash = htp->ht_gethash(k);
+	BUG_ON(htep->hte_hash != lsp->hls_hash[new]);
 	htbp = ht_get_bucket(&htp, k, &b, &i);		//\lnlbl{add:get_curbucket}
+	BUG_ON(htbp != lsp->hbp[new]);
+	BUG_ON(i != lsp->hls_idx[new]);
 	cds_list_add_rcu(&htep->hte_next[i], &htbp->htb_head); //\lnlbl{add:add}
 }							//\lnlbl{add:e}
 
