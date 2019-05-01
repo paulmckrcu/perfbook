paulmckrcu/perfbook

commit 29d0c7c185f2f47af70e27795dc011940e7345f7

toolsoftrade: Add labels in code samples as comments
For referencing from text, put labels at the end of lines as
comments in code samples.

Labels look like the following (in the case of C source):

  ... //%lnlbl[<line label>]

, or

  ... //$lnlbl@<line label>^

, or other choices of escape charactors depending to
the charactor set used in a particular code snippet.

In the first example, "%", "[", and "]" are the escape
charactors to be replaced with "\", "{", and "}", respectively.
In the second example, "$", "@", and "^" are selected as
escape charactors.

By escape-to-LaTeX feature of "fancyvrb"
package, both examples will be equivalent to:

  ... \lnlbl{<line label>}

with " //" removed by a conversion script (to be added
in a following commit) in "Verbatim" environment of LaTeX
sources.

\lnlbl{} macro is to be defined in preamble of perfbook.tex
so that the actual label has a form with a prefix string
"ln:<chapter>:<file name>:<function>:".

Any sets of three charactors are good as escape charactors
as long as they don't appear in a code snippet.
Other candidates of escape charactors would be:

    "\", "#", "!", "|", "`", "~"

Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

CodeSamples/api-pthreads/api-pthreads.h
@@ -286,19 +286,21 @@ static __inline__ void wait_all_threads(void)
  */
 static __inline__ void waitall(void)
 {
+// \begin{snippet}[labelbase=ln:api-pthreads:api-pthreads:waitall,commandchars=\%\[\]]
 	int pid;
 	int status;
 
-	for (;;) {
-		pid = wait(&status);
+	for (;;) {				//\lnlbl{loopa}
+		pid = wait(&status);		//\lnlbl{wait}
 		if (pid == -1) {
-			if (errno == ECHILD)
-				break;
-			perror("wait");
-			exit(EXIT_FAILURE);
+			if (errno == ECHILD)	//\lnlbl{ECHILD}
+				break;		//\lnlbl{break}
+			perror("wait");		//\lnlbl{perror}
+			exit(EXIT_FAILURE);	//\lnlbl{exit}
 		}
-		poll(NULL, 0, 1);
-	}
+		poll(NULL, 0, 1);		//\fcvexclude
+	}					//\lnlbl{loopb}
+// \end{snippet}
 }
 
 static __inline__ void run_on(int cpu)

CodeSamples/toolsoftrade/forkjoinvar.c
@@ -24,6 +24,7 @@
 #include <errno.h>
 #include "../api.h"
 
+// \begin{snippet}[labelbase=ln:toolsoftrade:forkjoinvar:main,commandchars=\$\@\^]
 int x = 0;
 
 int main(int argc, char *argv[])
@@ -32,9 +33,9 @@ int main(int argc, char *argv[])
 
 	pid = fork();
 	if (pid == 0) { /* child */
-		x = 1;
-		printf("Child process set x=1\n");
-		exit(EXIT_SUCCESS);
+		x = 1;					//\lnlbl{setx}
+		printf("Child process set x=1\n");	//\lnlbl{print:c}
+		exit(EXIT_SUCCESS);			//\lnlbl{exit:s}
 	}
 	if (pid < 0) { /* parent, upon error */
 		perror("fork");
@@ -43,8 +44,9 @@ int main(int argc, char *argv[])
 
 	/* parent */
 
-	waitall();
-	printf("Parent process sees x=%d\n", x);
+	waitall();					//\lnlbl{waitall}
+	printf("Parent process sees x=%d\n", x);	//\lnlbl{print:p}
 
 	return EXIT_SUCCESS;
 }
+// \end{snippet}

CodeSamples/toolsoftrade/pcreate.c
@@ -26,6 +26,7 @@
 #include <errno.h>
 #include "../api.h"
 
+// \begin{snippet}[labelbase=ln:toolsoftrade:pcreate:mythread,commandchars=\$\@\^]
 int x = 0;
 
 void *mythread(void *arg)
@@ -41,14 +42,15 @@ int main(int argc, char *argv[])
 	pthread_t tid;
 	void *vp;
 
-	if ((en = pthread_create(&tid, NULL, mythread, NULL)) != 0) {
+	if ((en = pthread_create(&tid, NULL,		//\lnlbl{create:a}
+	                         mythread, NULL)) != 0) {//\lnlbl{create:b}
 		fprintf(stderr, "pthread_join: %s\n", strerror(en));
 		exit(EXIT_FAILURE);
 	}
 
 	/* parent */
 
-	if ((en = pthread_join(tid, &vp)) != 0) {
+	if ((en = pthread_join(tid, &vp)) != 0) {	//\lnlbl{join}
 		fprintf(stderr, "pthread_join: %s\n", strerror(en));
 		exit(EXIT_FAILURE);
 	}
@@ -56,3 +58,4 @@ int main(int argc, char *argv[])
 
 	return EXIT_SUCCESS;
 }
+// \end{snippet}

commit 7816f2cf453018b3756851487a4cef4355157996

toolsoftrade: Fix extraction range of waitall()
Commit 29d0c7c185f2 ("toolsoftrade: Add labels in code samples as
comments") failed to include the function name and the closing brace
in the snippet of waitall() extracted from api-pthreads.h.

The original snippet removed in commit a62cf8469280 ("toolsoftrade:
Reference line in code snippets by label") contained those
lines.

Fix it by moving meta commands in api-pthreads.h

Fixes: 29d0c7c185f2 ("toolsoftrade: Add labels in code samples as comments")
Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
Signed-off-by: Paul E. McKenney <paulmck@linux.ibm.com>

CodeSamples/api-pthreads/api-pthreads.h
@@ -287,9 +287,9 @@ static __inline__ void wait_all_threads(void)
 /*
  * Wait on all child processes.
  */
+// \begin{snippet}[labelbase=ln:api-pthreads:api-pthreads:waitall,commandchars=\%\[\]]
 static __inline__ void waitall(void)
 {
-// \begin{snippet}[labelbase=ln:api-pthreads:api-pthreads:waitall,commandchars=\%\[\]]
 	int pid;
 	int status;
 
@@ -303,8 +303,8 @@ static __inline__ void waitall(void)
 		}
 		poll(NULL, 0, 1);		//\fcvexclude
 	}					//\lnlbl{loopb}
-// \end{snippet}
 }
+// \end{snippet}
 
 static __inline__ void run_on(int cpu)
 {