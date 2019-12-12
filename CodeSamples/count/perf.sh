5ee562ff33a1766e00c6751b303db066966f47d5
Paul E. McKenney <paulmck@kernel.org>
2019-12-11 17:29:05 -0800

    count: Update perf.sh for larger systems
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- CodeSamples/count/perf.sh
+++ CodeSamples/count/perf.sh
@@ -21,11 +21,11 @@
 #
 # Authors: Paul E. McKenney <paulmck@kernel.org>
 
-nsamples=5
+nsamples=30
 
 for count in count_atomic count_end count_end_rcu count_lim count_lim_app count_lim_atomic count_lim_sig count_limd count_nonatomic count_stat count_stat_atomic count_stat_eventual
 do
-	for ncpu in 1 2 3 4 5 6 7 8 10 12 14 16 20 24 28 32 40 48 56 64
+	for ncpu in 1 2 3 4 5 6 7 8 10 12 14 16 20 24 28 32 40 48 56 64 80 96 112 128 160 192 224 256 320 384 420
 	do
 		for ((i = 0; i < $nsamples; i++))
 		do
@@ -34,7 +34,7 @@ do
 			sleep 1
 		done
 	done
-	for ncpu in 1 2 3 4 5 6 7 8 10 12 14 16 20 24 28 32 40 48 56 64
+	for ncpu in 1 2 3 4 5 6 7 8 10 12 14 16 20 24 28 32 40 48 56 64 80 96 112 128 160 192 224 256 320 384 420
 	do
 		for ((i = 0; i < $nsamples; i++))
 		do
