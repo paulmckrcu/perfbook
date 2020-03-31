paulmckrcu/perfbook

commit 882f03c84dd4a737ccbc9d51cd7fc9eee9b63117

qqz.sty: Add background color to Quiz part of Quick Quizzes
To demonstrate the use of framed background for quiz part of Quick
Quizzes, add a boolean "qqzbg" and enable it for new experimental
targets "2cqq" and "1cqq".

Also enable it in existing targets "msnt" and "1csf".

Warning: Enabling qqzbg will increase the build time by as much as 50%.

Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

Makefile
@@ -16,7 +16,7 @@ LST_SOURCES := $(wildcard CodeSamples/formal/promela/*.lst) \
 
 LATEXGENERATED = autodate.tex qqz.tex contrib.tex origpub.tex
 
-ABBREVTARGETS := tcb 1c hb msns mss mstx msr msn msnt 1csf
+ABBREVTARGETS := tcb 1c hb msns mss mstx msr msn msnt 1csf 2cqq 1cqq
 
 PDFTARGETS := perfbook.pdf $(foreach v,$(ABBREVTARGETS),perfbook-$(v).pdf)
 
@@ -275,6 +275,7 @@ ifeq ($(NIMBUSMONO),)
 	$(error Font package 'nimbus15' not found. See #9 in FAQ-BUILD.txt)
 endif
 	sed -e 's/%msfontstub/\\usepackage[zerostyle=a]{newtxtt}/' \
+	    -e 's/{qqzgb}{false}/{qqzbg}{true}/' \
 	    -e 's/{nimbusavail}{false}/{nimbusavail}{true}/' < $< > $@
 
 perfbook-1csf.tex: perfbook-1c.tex
@@ -288,8 +289,15 @@ ifeq ($(NIMBUSMONO),)
 	$(error Font package 'nimbus15' not found. See #9 in FAQ-BUILD.txt)
 endif
 	sed -e 's/setboolean{sansserif}{false}/setboolean{sansserif}{true}/' \
+	    -e 's/{qqzbg}{false}/{qqzbg}{true}/' \
 	    -e 's/{nimbusavail}{false}/{nimbusavail}{true}/' \
 	    -e 's/%msfontstub/\\usepackage[var0]{inconsolata}[2013\/07\/17]/' < $< > $@
+
+perfbook-2cqq.tex: perfbook.tex
+	sed -e 's/{qqzbg}{false}/{qqzbg}{true}/' < $< > $@
+
+perfbook-1cqq.tex: perfbook-1c.tex
+	sed -e 's/{qqzbg}{false}/{qqzbg}{true}/' < $< > $@
 
 # Rules related to perfbook_html are removed as of May, 2016
 
@@ -398,6 +406,8 @@ help:
 	@echo
 	@echo "Experimental targets:"
 	@echo "  Full,              Abbr."
+	@echo "  perfbook-2cqq,     2cqq: 2c with Quick Quiz background"
+	@echo "  perfbook-1cqq,     1cqq: 1c with Quick Quiz background"
 	@echo "  perfbook-tcb,      tcb:  2c with table caption at bottom (prev default)"
 	@echo "  perfbook-msnt.pdf, msnt: 2c with newtxtt as monospace (non-slashed 0)"
 	@echo "  perfbook-mstx.pdf, mstx: 2c with txtt as monospace"

perfbook.tex
@@ -68,6 +68,7 @@
 \SetWatermarkHorCenter{.85\paperwidth}
 \SetWatermarkVerCenter{.95\paperheight}
 \SetWatermarkText{\texttt{\commitid}}
+\usepackage[breakable,skins]{tcolorbox}
 \usepackage[bookmarks=true,bookmarksnumbered=true,pdfborder={0 0 0},linktoc=all]{hyperref}
 \usepackage{footnotebackref} % to enable cross-ref of footnote
 \usepackage[all]{hypcap} % for going to the top of figure and table
@@ -84,6 +85,9 @@
 \titleformat{\paragraph}[runin]{\normalfont\normalsize\bfseries}{}{0pt}{}
 
 % custom packages
+\newboolean{qqzbg}
+\setboolean{qqzbg}{false}
+\newcommand{\IfQqzBg}[2]{\ifthenelse{\boolean{qqzbg}}{#1}{#2}}
 
 \usepackage{qqz}
 \usepackage{origpub}

qqz.sty
@@ -20,6 +20,7 @@
 %%          Paolo Bonzini <pbonzini@redhat.com> # cross-link questions and answers
 %%          Akira Yokosawa <akiyks@gmail.com>   # improvement of cross-link
 
+\tcbset{enhanced,colback=black!5,boxrule=0.5pt,top=1pt,bottom=1pt,left=0pt,right=0pt}
 \newcounter{quickquizctr}[chapter]
 \newcounter{quickquizctrE}[chapter]
 \newcounter{quickquizctrC}[section]
@@ -32,6 +33,21 @@
 	\chapter{#2}
 	\label{#1}
 	}
+\IfQqzBg{
+\newcommand{\QuickQuiz}[1]{
+	\refstepcounter{quickquizctr}
+	\begin{tcolorbox}[breakable]
+	\small\QuickQHeading{QQ}{QQA}{\thechapter.\thequickquizctr}
+	{#1}
+}
+\newcommand{\QuickQuizAnswer}[1]{%
+	\refstepcounter{quickquizctrE}%
+	\hyperref[QQA.\thechapter.\thequickquizctr]{\rule{7pt}{7pt}}%
+	\label{QQE.\thechapter.\thequickquizctrE}
+	\end{tcolorbox}
+}
+\newcommand{\QuickQuizEnd}{}
+}{
 \newcommand{\QuickQuiz}[1]{
 	\goodbreak\refstepcounter{quickquizctr}%
 	\QuickQHeading{QQ}{QQA}{\thechapter.\thequickquizctr}
@@ -41,6 +57,7 @@
 	\hyperref[QQA.\thechapter.\thequickquizctr]{\rule{7pt}{7pt}}%
 	\label{QQE.\thechapter.\thequickquizctrE}}
 \newcommand{\QuickQuizEnd}{\goodbreak}
+}
 
 % To create a macro referencing the previously defined quick quiz:
 %	\QuickQuizLabel{\QQname}
@@ -78,6 +95,18 @@
 	\edef\QuickQuizAnswerChapter{\getrefnumber{#1}}
 	\section{#2}}
 
+\IfQqzBg{
+\newcommand{\QuickQ}[1]{
+	\refstepcounter{quickquizctrC}
+	\begin{tcolorbox}[breakable]
+	\QuickQHeading{QQA}{QQ}{\QuickQuizAnswerChapter.\thequickquizctrC} \\
+}
+
+\newcommand{\QuickA}[1]{%
+	\hyperref[QQ.\QuickQuizAnswerChapter.\thequickquizctrC]{\rule{7pt}{7pt}}
+	\end{tcolorbox}
+	\noindent\textbf{Answer:} \\ }
+}{
 \newcommand{\QuickQ}[1]{
 	\par
 	\refstepcounter{quickquizctrC}%
@@ -88,9 +117,9 @@
 	\hyperref[QQ.\QuickQuizAnswerChapter.\thequickquizctrC]{\rule{7pt}{7pt}}
 	\par\vspace{0.2\baselineskip plus .2ex minus .1ex}
 	\noindent\textbf{Answer:} \\ }
+}
 
 \newcommand{\QuickE}{%
 	\hyperref[QQE.\QuickQuizAnswerChapter.\thequickquizctrC]{\ding{113}}
 	\goodbreak\vspace{0.7\baselineskip plus .5ex minus .2ex}
 }
-

commit 1641c6ae8fa83f0c079ac3ccb48b4fce424e6eb4

Makefile: Fix typo in recipe of msnt
Fixes: 882f03c84dd4 ("qqz.sty: Add background color to Quiz part of Quick Quizzes")
Signed-off-by: Akira Yokosawa <akiyks@gmail.com>

Makefile
@@ -294,7 +294,7 @@ ifeq ($(NIMBUSMONO),)
 	$(error Font package 'nimbus15' not found. See #9 in FAQ-BUILD.txt)
 endif
 	sed -e 's/%msfontstub/\\usepackage[zerostyle=a]{newtxtt}/' \
-	    -e 's/{qqzgb}{false}/{qqzbg}{true}/' \
+	    -e 's/{qqzbg}{false}/{qqzbg}{true}/' \
 	    -e 's/{nimbusavail}{false}/{nimbusavail}{true}/' < $< > $@
 
 perfbook-sf.tex: $(PERFBOOK_BASE)