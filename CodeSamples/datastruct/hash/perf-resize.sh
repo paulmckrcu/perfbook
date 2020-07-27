355b41df846fbd35e719a3fe9b8dd05aceedec09
Paul E. McKenney <paulmck@kernel.org>
2020-07-26 19:27:23 -0700

    datastruct: Make resizable hash table script sense CPUs
    Currently, perf-resize.sh is hard-coded to run on a 64-CPU system, which
    is fine when you happen to be running on a 64-CPU system.  This commit
    therefore makes this script auto-adjust to the number of CPUs.
    
    Later commits will pull the new bash functions into a common-code bash
    "library".
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- CodeSamples/datastruct/hash/perf-resize.sh
+++ CodeSamples/datastruct/hash/perf-resize.sh
@@ -17,31 +17,94 @@
 #
 # Authors: Paul E. McKenney <paulmck@kernel.org>
 
-nsamples=7
+nsamples=30
 
+# There are about five buckets per 64-byte cache line, and up to about
+# 448 CPUs.  Giving each CPU its own cache line in a deterministic workload
+# requires 448*5=2240 cache lines.  But in this case, there is a very high
+# probability of cacheline collision.  Multiplying the number of buckets
+# by 100 (22400) reduces the per-access chance of cacheline collision to
+# roughly 1% (computing the exact value is left as an exercise).  Rounding
+# to the next power of two gets 262,144.  So use this and 524,288.
+#
+# But use the actual maximum number of CPUs instead of hard-coding 448.
+# Also round down because laziness.
+
+# Power of two, rounding down and up, respectively.
+power2down() {
+	echo $1 |
+	awk '
+	{
+		x = $1;
+		logx = log(x) / log(2);
+		ilogx = int(logx);
+		print 2^ilogx;
+	}'
+}
+power2up() {
+	echo $1 |
+	awk '
+	{
+		x = $1;
+		logx = log(x) / log(2);
+		ilogx = int(logx);
+		if (x == 2^ilogx)
+			print 2^ilogx;
+		else
+			print 2 * 2^ilogx;
+	}'
+}
+
+# Iteration increment given last number used and divisor (which should
+# be a power of two).  If the result would be fractional, it is rounded up
+# to 1.  So "power2inc 64 1" will give 64 (one sample per power of two),
+# and "power2inc 64 8" will give 8 (eight samples per power of two).
+power2inc() {
+	awk -v iter=$1 -v p2div=$2 -v p2raw="`power2down $1`" < /dev/null '
+	BEGIN {
+		if (p2raw <= p2div)
+			print 1;
+		else
+			print p2raw / p2div;
+	}'
+}
+
+maxcpu="`grep '^processor' /proc/cpuinfo | tail -1 | awk '{ print $3 }'`"
+maxcpu=$((maxcpu + 1))
+lastcpu=${1-$maxcpu}
+if test $lastcpu -gt $maxcpu
+then
+	echo $0: Specified $lastcpu CPUs, only $maxcpu available!!!
+	exit 
+fi
+nbucketslo=$((lastcpu*5*100))
+nbucketslo=`power2up $nbucketslo`
+nbucketshi=$((nbucketslo*2))
 
 # Simple hash tables, read-only.
-for ncpu in 1 4 8 16 24 32 48 60
+for ((i = 0; i < $nsamples; i++))
 do
-	for epw in 2048 16384 131072
+	for epwmult in '/8' '' '*8'
 	do
-		for ((i = 0; i < $nsamples; i++))
+		epwlo=$((nbucketslo$epwmult))
+		epwhi=$((nbucketshi$epwmult))
+		ncpu=1
+		while test $ncpu -le $lastcpu
 		do
-			echo hash_bkt_rcu --perftest --nreaders $ncpu --nbuckets 1024 --elems/writer $epw --duration 1000 --updatewait 0
-			./hash_bkt_rcu --perftest --nreaders $ncpu --nbuckets 1024 --elems/writer $epw --duration 1000 --updatewait 0
+			echo hash_bkt_rcu --perftest --nreaders $ncpu --nbuckets $nbucketslo --elems/writer $epwlo --duration 1000 --updatewait 0
+			./hash_bkt_rcu --perftest --nreaders $ncpu --nbuckets $nbucketslo --elems/writer $epwlo --duration 1000 --updatewait 0
 			sleep 1
-		done
-		for ((i = 0; i < $nsamples; i++))
-		do
-			echo hash_resize --perftest --nreaders $ncpu --nbuckets 1024 --elems/writer $epw --resizemult 2 --duration 1000 --updatewait 0
-			./hash_resize --perftest --nreaders $ncpu --nbuckets 1024 --elems/writer $epw --resizemult 2 --duration 1000 --updatewait 0
+
+			echo hash_resize --perftest --nreaders $ncpu --nbuckets $nbucketslo --elems/writer $epwlo --resizemult 2 --duration 1000 --updatewait 0
+			./hash_resize --perftest --nreaders $ncpu --nbuckets $nbucketslo --elems/writer $epwlo --resizemult 2 --duration 1000 --updatewait 0
 			sleep 1
-		done
-		for ((i = 0; i < $nsamples; i++))
-		do
-			echo hash_bkt_rcu --perftest --nreaders $ncpu --nbuckets 2048 --elems/writer $epw --duration 1000 --updatewait 0
-			./hash_bkt_rcu --perftest --nreaders $ncpu --nbuckets 2048 --elems/writer $epw --duration 1000 --updatewait 0
+
+			echo hash_bkt_rcu --perftest --nreaders $ncpu --nbuckets $nbucketshi --elems/writer $epwhi --duration 1000 --updatewait 0
+			./hash_bkt_rcu --perftest --nreaders $ncpu --nbuckets $nbucketshi --elems/writer $epwhi --duration 1000 --updatewait 0
 			sleep 1
+
+			incr=`power2inc $ncpu 8`
+			ncpu=$((ncpu + incr))
 		done
 	done
 done
