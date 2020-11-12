c710e85f324db13707672d1d2cd0d8411c3df260
Paul E. McKenney <paulmck@kernel.org>
2020-11-12 11:33:36 -0800

    CodeSamples/defer: Adjust rcuscale.sh to allow for renaming
    The Linux kernel's rcuperf.sh file has been renamed to rcuscale.sh,
    so this commit adjusts perfbook's rcuscale.sh accordingly.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- CodeSamples/defer/rcuscale.sh
+++ CodeSamples/defer/rcuscale.sh
@@ -26,18 +26,18 @@
 #
 # Copyright (c) 2020 Paul E. McKenney, Facebook.
 
-if test -r kernel/rcu/refperf*.c > /dev/null 2>&1
+if test -r kernel/rcu/refscale*.c > /dev/null 2>&1
 then
 	:
 else
-	echo No 'rcuperf*.c': Need recent Linux kernel source tree.
+	echo No 'refscale*.c': Need recent Linux kernel source tree.
 	exit 1
 fi
-if test -x tools/testing/selftests/rcutorture/bin/kvm-recheck-refperf*.sh > /dev/null 2>&1
+if test -x tools/testing/selftests/rcutorture/bin/kvm-recheck-refscale*.sh > /dev/null 2>&1
 then
 	:
 else
-	echo No 'kvm-recheck-rcuperf*.c': Need recent Linux kernel source tree.
+	echo No 'kvm-recheck-rcuscale*.c': Need recent Linux kernel source tree.
 	exit 1
 fi
 
@@ -46,7 +46,7 @@ trap 'rm -f $T' 0 2
 
 maxcpu="`grep '^processor' /proc/cpuinfo | tail -1 | awk '{ print $3 }'`"
 lastcpu=${1-$maxcpu}
-primlist="`grep '\.name[ 	]*=' kernel/rcu/refperf*.c |
+primlist="`grep '\.name[ 	]*=' kernel/rcu/refscale*.c |
 	   sed -e 's/^[^"]*"//' -e 's/".*$//'`"
 if ((lastcpu > 100))
 then
@@ -71,7 +71,7 @@ do
 	fi
 	for prim in $primlist
 	do
-		taskset -c $cpulist tools/testing/selftests/rcutorture/bin/kvm.sh --cpus $ncpus --duration 10 --torture refperf --configs NOPREEMPT --bootargs "refperf.perf_type=$prim refperf.nreaders=$ncpus refperf.loops=10000 refperf.holdoff=$holdoff" --kconfig "CONFIG_NR_CPUS=$((lastcpu + 1))" --trust-make > $T 2>&1
+		taskset -c $cpulist tools/testing/selftests/rcutorture/bin/kvm.sh --cpus $ncpus --duration 10 --torture refscale --configs NOPREEMPT --bootargs "refscale.scale_type=$prim refscale.nreaders=$ncpus refscale.loops=10000 refscale.holdoff=$holdoff" --kconfig "CONFIG_NR_CPUS=$((lastcpu + 1))" --trust-make > $T 2>&1
 		ret=$?
 		fstr=""
 		resdir="`grep -m 1 '^Results directory: ' $T | sed -e 's/^Results directory: *//'`"
