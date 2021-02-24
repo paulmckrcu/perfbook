2060b54a52ffaaea818807f7e602a168d46c4d50
Paul E. McKenney <paulmck@kernel.org>
2021-02-23 21:11:55 -0800

    advsync: Add LIFO Push and pointer zap
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- CodeSamples/advsync/lifo-push.c
+++ CodeSamples/advsync/lifo-push.c
@@ -21,29 +21,29 @@ struct node_t* top;				//\lnlbl{top}
 
 void list_push(value_t v)			//\lnlbl{push:b}
 {
-	struct node_t *newnode = malloc(sizeof(*newnode));
+	struct node_t *newnode = malloc(sizeof(*newnode)); //\lnlbl{push:alloc}
 	struct node_t *oldtop;
 
-	newnode->val = v;
+	newnode->val = v;			//\lnlbl{push:initialize}
 	oldtop = READ_ONCE(top);
 	do {
-		newnode->next = oldtop;
-		oldtop = cmpxchg(&top, newnode->next, newnode);
-	} while (newnode->next != oldtop);
+		newnode->next = oldtop;		//\lnlbl{push:next}
+		oldtop = cmpxchg(&top, newnode->next, newnode); //\lnlbl{push:cmpxchg}
+	} while (newnode->next != oldtop);	//\lnlbl{push:check}
 }						//\lnlbl{push:e}
 
 
 void list_pop_all(void (foo)(struct node_t *p))	//\lnlbl{popall:b}
 {
-	struct node_t *p = xchg(&top, NULL);
+	struct node_t *p = xchg(&top, NULL);	//\lnlbl{popall:xchg}
 
-	while (p) {
-		struct node_t *next = p->next;
+	while (p) {				//\lnlbl{popall:loop:b}
+		struct node_t *next = p->next;	//\lnlbl{popall:next}
 
-		foo(p);
-		free(p);
-		p = next;
-	}
+		foo(p);				//\lnlbl{popall:foo}
+		free(p);			//\lnlbl{popall:free}
+		p = next;			//\lnlbl{popall:pnext}
+	}					//\lnlbl{popall:loop:e}
 }						//\lnlbl{popall:e}
 //\end{snippet}
 
