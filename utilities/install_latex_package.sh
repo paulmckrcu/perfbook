0d6aa26c6b4c5577ef15b228d060f9ad6fe1f96c
SeongJae Park <sj38.park@gmail.com>
2021-03-27 12:21:23 +0100

    utilities: Add a script for automated latex package install
    Installing version mismatch latex packages one by one in manual, as
    described in #10.A in FAQ-BUILD.txt is cumbersome.  This commit adds a
    script for automation of the work.
    
    Acked-by: Akira Yokosawa <akiyks@gmail.com>
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

---
+++ utilities/install_latex_package.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <package name>"
+	exit 1
+fi
+
+package=$1
+
+packages="titlesec cleveref listings draftwatermark epigraph fvextra"
+supported="false"
+for p in $packages
+do
+	if [ "$package" == "$p" ]
+	then
+		supported="true"
+		break
+	fi
+done
+if [ "$supported" == "false" ]
+then
+	echo "$package is not supported"
+	exit 1
+fi
+
+wget "http://mirrors.ctan.org/macros/latex/contrib/$package.zip"
+unzip "$package.zip"
+cd "$package" || exit 1
+
+latex "$package.ins"
+if [ ! -e "$package.sty" ]
+then
+	echo "$package.sty creation failed"
+	exit 1
+fi
+
+install_dir=~/texmf/tex/latex/"$package"
+mkdir -p "$install_dir"
+cp "$package.sty" "$install_dir/"
+texhash ~/texmf
+
+cd .. || exit 1
+rm -fr "$package" "$package.zip"
