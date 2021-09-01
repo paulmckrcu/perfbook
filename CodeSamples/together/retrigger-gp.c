d453cbfb840d9577100b852c0a921508d2253412
Akira Yokosawa <akiyks@gmail.com>
2021-09-01 22:10:10 +0900

    together/applyrcu: Apply \clnref{} scheme to Listing 10.13
    Embedding line labels in code snippets and referencing them by
    \clnref{} and its variants can reduce the risk of line-count mismatch
    in the future.
    
    Also fix a typo (RCLOSING -> RECLOSING).
    
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- CodeSamples/together/retrigger-gp.c
+++ CodeSamples/together/retrigger-gp.c
@@ -1,32 +1,32 @@
 //\begin{snippet}[labelbase=ln:together:retrigger-gp:whole,commandchars=\\\@\$,tabsize=8]
-#define RTRG_CLOSED    0
+#define RTRG_CLOSED    0	//\lnlbl{states:b}
 #define RTRG_OPEN      1
 #define RTRG_CLOSING   2
 #define RTRG_REOPENING 3
-#define RTRG_RECLOSING 4
+#define RTRG_RECLOSING 4	//\lnlbl{states:e}
 
-int rtrg_status;
-DEFINE_SPINLOCK(rtrg_lock);
+int rtrg_status;		//\lnlbl{status}
+DEFINE_SPINLOCK(rtrg_lock);	//\lnlbl{lock}
 struct rcu_head rtrg_rh;
 
-void close_cb(struct rcu_head *rhp)
+void close_cb(struct rcu_head *rhp)		//\lnlbl{close_cb:b}
 {
 	spin_lock(rtrg_lock);
 	if (rtrg_status = RTRG_CLOSING) {
-		close_cleanup();
+		close_cleanup();		//\lnlbl{cleanup}
 		rtrg_status = RTRG_CLOSED;
 	} else if (rtrg_status == RTRG_REOPENING) {
 		rtrg_status = RTRG_OPEN;
 	} else if (rtrg_status == RTRG_RECLOSING) {
 		rtrg_status = RTRG_CLOSING;
-		call_rcu(&rtrg_rh, close_cb);
+		call_rcu(&rtrg_rh, close_cb);	//\lnlbl{call_rcu1}
 	} else {
 		WARN_ON_ONCE(1);
 	}
 	spin_unlock(rtrg_lock);
-}
+}						//\lnlbl{close_cb:e}
 
-int open(void)
+int open(void)					//\lnlbl{open:b}
 {
 	spin_lock(rtrg_lock);
 	if (rtrg_status == RTRG_CLOSED) {
@@ -38,23 +38,23 @@ int open(void)
 		spin_unlock(rtrg_lock);
 		return -EBUSY;
 	}
-	do_open();
+	do_open();				//\lnlbl{do_open}
 	spin_unlock(rtrg_lock);
-}
+}						//\lnlbl{open:e}
 
-int close(void)
+int close(void)					//\lnlbl{close:b}
 {
 	spin_lock(rtrg_lock);
 	if (rtrg_status == RTRG_OPEN) {
 		rtrg_status = RTRG_CLOSING;
-		call_rcu(&rtrg_rh, close_cb);
+		call_rcu(&rtrg_rh, close_cb);	//\lnlbl{call_rcu2}
 	} else if (rtrg_status == RTRG_REOPENING) {
 		rtrg_status = RTRG_RECLOSING;
 	} else {
 		spin_unlock(rtrg_lock);
 		return -ENOENT;
 	}
-	do_close();
+	do_close();				//\lnlbl{do_close}
 	spin_unlock(rtrg_lock);
-}
+}						//\lnlbl{close:e}
 //\end{snippet}
