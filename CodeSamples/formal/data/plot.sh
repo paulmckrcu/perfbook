ef431a48f646d11cf10c08b34cf59d305135641d
Akira Yokosawa <akiyks@gmail.com>
2022-03-23 11:37:18 +0900

    formal: Update RCU-test-ratio table as of v5.17
    Routine update of Figure 12.4.
    Total LOC of RCU + test has exceeded 30000 (30058) in v5.17.
    The range of y-axis (LOC) in plot.sh has been widened to [0,35000].
    
    Signed-off-by: Akira Yokosawa <akiyks@gmail.com>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- CodeSamples/formal/data/plot.sh
+++ CodeSamples/formal/data/plot.sh
@@ -21,7 +21,7 @@ set ytics out
 set y2tics 1 10
 set y2tics out
 set ytics nomirror
-set yrange [0:30000]
+set yrange [0:35000]
 set y2range [0:50]
 set style data histogram
 set style histogram rowstacked
