e63ea9461823aa1151e5322edeb3f2fe9a61429c
Paul E. McKenney <paulmck@kernel.org>
2023-12-12 20:46:40 -0800

    CodeSamples/cpu: Add fre.sh to reduce temporal data
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

---
+++ CodeSamples/cpu/fre.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Produce and reduce temporal fre data.
+#
+# Usage: bash fre.sh [ nthreads ]
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, you can access it online at
+# http://www.gnu.org/licenses/gpl-2.0.html.
+#
+# Copyright (C) Facebook, 2020
+#
+# Authors: Paul E. McKenney <paulmck@kernel.org>
+./temporal --fre --nthreads ${1-15} |
+awk '
+/^Write/ {
+	et = $4;
+	for (i in st) {
+		print i, st[i], et, et - st[i] (et < st[i] ? "!!!" : "");
+	}
+}
+
+$3 == 0 {
+	st[$1] = $2;
+}'
