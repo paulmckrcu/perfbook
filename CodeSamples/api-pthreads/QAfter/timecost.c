b203fcdc0877f6b9503a30279643120ac5f37301
Paul E. McKenney <paulmck@kernel.org>
2024-10-17 16:56:37 -0700

    api-pthreads/QAfter: Add comments to timecost
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- CodeSamples/api-pthreads/QAfter/timecost.c
+++ CodeSamples/api-pthreads/QAfter/timecost.c
@@ -1,6 +1,23 @@
 /*
  * Measure the overhead of several clock_gettime() options.
  *
+ * Sample output:
+ *
+ * 0 CLOCK_REALTIME          granularity: 0.000000432  tries: 1
+ * 5 CLOCK_REALTIME_COARSE   granularity: 0.001000008  tries: 25933
+ * 1 CLOCK_MONOTONIC         granularity: 0.000000164  tries: 1
+ * 4 CLOCK_MONOTONIC_RAW     granularity: 0.000000140  tries: 1
+ * 6 CLOCK_MONOTONIC_COARSE  granularity: 0.001000009  tries: 38983
+ * 7 CLOCK_BOOTTIME          granularity: 0.000000087  tries: 1
+ *
+ * 0 CLOCK_REALTIME          7.69e-08 s (2.00e-08 -> 1.23e-07) reads: 100000
+ * 5 CLOCK_REALTIME_COARSE   2.45e-08 s (2.44e-08 -> 5.61e-08) reads: 100000
+ * 1 CLOCK_MONOTONIC         7.69e-08 s (7.69e-08 -> 1.08e-07) reads: 100000
+ * 4 CLOCK_MONOTONIC_RAW     7.89e-08 s (1.84e-08 -> 1.13e-07) reads: 100000
+ * 6 CLOCK_MONOTONIC_COARSE  2.45e-08 s (6.31e-09 -> 5.23e-08) reads: 100000
+ * 7 CLOCK_BOOTTIME          7.70e-08 s (2.46e-08 -> 1.12e-07) reads: 100000
+ * x86_64 rdtsc              3.17e-08 s (8.15e-09 -> 5.42e-08) reads: 100000
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -30,6 +47,7 @@ int nsamples = NSAMPLES;
 int nsubsamples = NSUBSAMPLES;
 int cooldown = COOLDOWN;
 
+// Helper function for qsort().
 int dblcmp(const void *a_in, const void *b_in)
 {
 	double a = *(double *)a_in;
@@ -42,6 +60,7 @@ int dblcmp(const void *a_in, const void *b_in)
 	return 0;
 }
 
+// Extract minimum, median, and maximum from the specified array.
 void getstats(double *x, int xn, double *dtmin, double *dtmed, double *dtmax)
 {
 	int i;
@@ -75,6 +94,7 @@ static __always_inline unsigned long long rdtsc(void)
 	return EAX_EDX_VAL(val, low, high);
 }
 
+// Collect measurements of the architure-specific timebase.
 static void measure_overhead_arch(void)
 {
 	double dtdiff;
@@ -141,6 +161,7 @@ static char *clocknames[] = {
 	"CLOCK_BOOTTIME         ",
 };
 
+// Measure time required for the specified clocksource to tick.
 static void measure_granularity(int cidx)
 {
 	clockid_t c = clocks[cidx];
@@ -165,6 +186,7 @@ static void measure_granularity(int cidx)
 	//        t1.tv_sec, t1.tv_nsec, t2.tv_sec, t2.tv_nsec);
 }
 
+// Measure times required for each clocksource to tick.
 void measure_granularities(void)
 {
 	int c;
@@ -174,6 +196,7 @@ void measure_granularities(void)
 	}
 }
 
+// Measure the access overhead of the specified clocksource.
 static void measure_overhead(int cidx)
 {
 	clockid_t c = clocks[cidx];
@@ -203,7 +226,7 @@ static void measure_overhead(int cidx)
 		assert(!retval);
 		dtdiff = timespecs2double(&ts2, &ts1);
 		dts[i] = dtdiff / (double)j;
-		poll(NULL, 0, cooldown);
+		poll(NULL, 0, cooldown);  // Let the chip cool down.
 	}
 	getstats(dts, i, &dtmin, &dtmed, &dtmax);
 	printf("%d %s %#8.3g s (%#8.3g -> %#8.3g) reads: %d\n",
@@ -211,6 +234,7 @@ static void measure_overhead(int cidx)
 	free(dts);
 }
 
+// Measure the access overheads of each clocksource.
 static void measure_overheads(void)
 {
 	int c;
@@ -238,6 +262,7 @@ void usage(char *progname, const char *format, ...)
 	fprintf(stderr, "\t\tDefaults to %d.\n", NSAMPLES);
 	fprintf(stderr, "\t--nsubsamples\n");
 	fprintf(stderr, "\t\tNumber of measurements per sample.\n");
+	fprintf(stderr, "\t\tMaking this too large defeats --cooldown.\n");
 	fprintf(stderr, "\t\tDefaults to %d.\n", NSUBSAMPLES);
 	exit(-1);
 }
